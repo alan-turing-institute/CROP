1987fb552d2d9846b59f901bd80682dee7ba4711
misspawty <flora.roumpani.11@ucl.ac.uk>
2019-12-06 14:43:38 +0000

    create solution and first postgresdb

--- test.py
+++ test.py
@@ -1,8 +1,33 @@
 import sys
 import pandas as pd
-import sqlalchemy as db
+import datetime
+import psycopg2
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 
-
-engine = db.create_engine('postgresql://postgresql:crops@localhost:5433/cropdb')
-connection = engine.connect()
 print (sys.version)
+
+#engine = create_engine('postgresql+psycopg2://user:password@hostname/database_name')
+engine = create_engine('postgresql://postgres:crop@localhost:5433/postgres')
+connection = engine.connect()
+
+#print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.sdatetime.now()))
+
+Base = declarative_base()
+
+class Sensor (Base):
+    __tablename__= 'sensor'
+    ID = Column (Integer, primary_key=True)
+    MODBUSID = Column (Integer)
+    TIMESTAMP = Column(DateTime, default=datetime.datetime.utcnow)
+    #TIME_CREATED = Column(DateTime(timezone=True), server_default=func.now())
+    #TIME_UPDATED = Column(DateTime(timezone=True), onupdate=func.now())
+    TEMPERATURE = Column(Integer)
+
+    def __repr__(self):
+        return "<Sensor(MODBUSID='{}', TIMESTAMP='{}', TEMPERATURE'{}')>"\
+                .format(self.MODBUSID, self.TIMESTAMP, self.TEMPERATURE)
+
+Base.metadata.create_all(engine)
