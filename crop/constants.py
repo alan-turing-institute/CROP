ffeb88e987051f030eca1ca5bf93f7f262e5b70d
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2020-01-15 16:39:31 +0000

    refactoring

--- crop/constants.py
+++ crop/constants.py
@@ -1,28 +1,34 @@
+"""
+A module for constants
+"""
 import os
-import sys
 
-#print (sys.version)
+# Create connection string
+SQL_ENGINE = "postgresql"
+SQL_DBNAME = "crop_db"
+SQL_DEFAULT_DBNAME ='postgres'
+SQL_USER = os.environ['SQL_USER']
+SQL_PASSWORD = os.environ['SQL_PASS']
+SQL_HOST = "localhost"
+SQL_PORT = "5432"
 
-'''Create connection string'''
-engine = "postgresql"
-dbname = "test"
-default_dbname ='postgres'
-user_name= "postgres"
-password ="crop"
-host = "localhost"
-port = "5433"
+SQL_CONNECTION_STRING = "%s://%s:%s@%s:%s" % (SQL_ENGINE, SQL_USER, SQL_PASSWORD, 
+                                                 SQL_HOST, SQL_PORT)
 
-connection_string = "%s://%s:%s@%s:%s/%s" % (engine,user_name,password,host,port,dbname )
-connection_string_defaultdb = "%s://%s:%s@%s:%s/%s" % (engine,user_name,password,host,port,default_dbname )
-#print (connection_string)
+SQL_CONNECTION_STRING_DEFAULT = "%s/%s" % (SQL_CONNECTION_STRING, SQL_DEFAULT_DBNAME)
+SQL_CONNECTION_STRING_CROP = "%s/%s" % (SQL_CONNECTION_STRING, SQL_DBNAME)
 
-'''Create table list'''
-tables= ["sensor", "sensortype", "location",  "readings", "advantix" ]
+# connection_string = "%s://%s:%s@%s:%s/%s" % (engine,user_name,password,host,port,dbname )
+# connection_string_defaultdb = "%s://%s:%s@%s:%s/%s" % (engine,user_name,password,host,port,default_dbname )
+# #print (connection_string)
 
-'''test datasets paths'''
-#gets the path of current working directory
-cwd = os.getcwd()
-sensor_types_path = cwd+"\\Data\\Sensortypes.csv"
-Sensors_path = cwd+"\\Data\\Sensors.csv"
-locations_path = cwd+"\\Data\\locations.csv"
-advantix_data_path =cwd+"\\Data\\Raw\\raw-20191127-pt01.csv"
+# '''Create table list'''
+# tables= ["sensor", "sensortype", "location",  "readings", "advantix" ]
+
+# '''test datasets paths'''
+# #gets the path of current working directory
+# cwd = os.getcwd()
+# sensor_types_path = cwd+"\\Data\\Sensortypes.csv"
+# Sensors_path = cwd+"\\Data\\Sensors.csv"
+# locations_path = cwd+"\\Data\\locations.csv"
+# advantix_data_path =cwd+"\\Data\\Raw\\raw-20191127-pt01.csv"
