f5f717c724b66c7342a840efb9f45cf784bdcdf6
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2020-01-15 16:39:31 +0000

    refactoring

--- tests/test_create_db.py
+++ tests/test_create_db.py
@@ -1,29 +1,19 @@
 import pytest
 
-from ..crop.create_db import create_database
+from ..crop.constants import (
+    SQL_DBNAME
+)
 
+from ..crop.create_db import (
+    create_database
+)
 
 def test_create_database():
 
     try:
-        create_database(None)
+        create_database(SQL_DBNAME)
     except:
         assert(False)
 
     assert(True)
-
-
-# #import createdb
-# import main
-
-# #check if files are loaded in the dataframes
-# def check(file):
-#     try:
-#         #main.Readings_Advantix ()
-#         print ("itworked")
-#     except AssertionError:
-#         assert False, "  cant read csv"
-
-# check(df)
-
 
---
+++ tests/test_populate_db.py
@@ -0,0 +1,5 @@
+import pytest
+
+def test_import_test_data():
+
+    assert(False)
