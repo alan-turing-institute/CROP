b3d41c6beb142da4de059d1a45f3659b19ab6b8e
misspawty <flora.roumpani.11@ucl.ac.uk>
2020-01-16 12:10:03 +0000

    updated structure, constants

--- crop/structure.py
+++ crop/structure.py
@@ -1,72 +1,96 @@
 """
-Module to define the structure of the database
-
+Module to define the structure of the database. Each Class, defines a table in the database.
+    __tablename__: creates the table with the name given
+    __table_args__: table arguments eg: __table_args__ = {'sqlite_autoincrement': True}
+    BASE: the declarative_base() callable returns a new base class from which all mapped classes
+    should inherit. When the class definition is completed, a new Table and mapper() is generated.
 """
 
 import datetime
 
-from sqlalchemy import (create_engine, ForeignKey, MetaData, Table, Float, Column, Integer, String, DateTime, Text)
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import (ForeignKey, Float, Column, Integer, String, DateTime, Text)
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 
-#Is used to tell SQL that the classes we create are db tables'''
+from constants import (
+    SENSOR_TABLE_NAME,
+    SENSOR_TYPE_TABLE_NAME,
+    LOCATION_TABLE_NAME,
+    ADVANTIX_READINGS_TABLE_NAME
+    )
+
+
 BASE = declarative_base()
 
-'''CLASSES WHICH SQLALCHEMY USES TO DEFINE TABLES AND COLUMNS IN THE DB'''
-'''Class Sensor contains a list of all the sensors in the farm'''
-class Sensor(BASE) :
-    #Tells SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
-    __tablename__= 'sensor'
-    #__table_args__ = {'sqlite_autoincrement': True}
+class Sensor(BASE):
+    """
+    This class contains a list of all the sensors in the farm
+    """
 
-    #tell SQLAlchemy the name of column and its attributes:
-    id = Column (Integer, primary_key =True) 
-    #TYPE= Column (String)
-    #TYPE= Column (String, ForeignKey('sensortype.TYPE'))
-    TYPE_ID= Column(Integer, ForeignKey('sensortype.id')) #many to one relationship, inherits the key from the other table
-    type= relationship("Type")  #defines that is is a relationship
+    __tablename__ = SENSOR_TABLE_NAME
 
-    LOCATION = relationship("Location") #one to one relationship
-    READINGS = relationship("Readings") #one to many relationship
+    id = Column(Integer, primary_key=True)
+    type_id = Column(Integer, ForeignKey(SENSOR_TYPE_TABLE_NAME+'.id')) #many to one relationship
+    type_relationship = relationship("Type")
+    location_relationship = relationship("Location") #one to many relationship
+    advantix_readings_relationship = relationship("Readings") #one to many relationship
+    installation_time = Column(DateTime, default=datetime.datetime.utcnow) #picks up current time.
 
-    INSTALLATIONTIME = Column(DateTime, default=datetime.datetime.utcnow) #picks up current time. 
 
-'''Class SensorType contains a list and characteristics of each type of sensor installed in the farm. eg. "Advantix" '''
 class Type(BASE):
-    __tablename__= 'sensortype'
+    """
+    This class contains a list and characteristics of each type of sensor installed eg. "Advantix"
+    """
+    __tablename__ = SENSOR_TYPE_TABLE_NAME
+
     id = Column(Integer, primary_key=True)
     #UUID =   Column(String(36), unique=True, nullable=False)
-    TYPE= Column(String)
-    description= Column (String)
-   
-'''Class Location describes all the physical location in the farm. eg. Sensor A is found in the front section, in the left column , in the 3rd self. ''' 
+    sensor_type = Column(String)
+    description = Column(Text)
+
+
 class Location(BASE):
-    __tablename__= 'location'
+    """
+    This class describes all the physical locations in the farm. eg. Sensor x is found in
+    the front section, in the left column , in the 3rd self.
+    """
 
-    ID = Column (Integer, primary_key=True)
-    Sensor_id =Column(Integer, ForeignKey ('sensor.id'))
-    sensor=relationship("Sensor", back_populates="sensors")
-    SECTION = Column(Integer) #A/B
-    COLUMN = Column (Integer) #no
-    SELVE = Column (Integer) #1-4
-    CODE = Column (String)
+    __tablename__ = LOCATION_TABLE_NAME
 
-'''Base class for the sensor Readings'''
+    id = Column(Integer, primary_key=True)
+    sensor_id = Column(Integer, ForeignKey(SENSOR_TABLE_NAME+'.id'))
+    sensor_relationship = relationship(Sensor, back_populates="sensors")
+    section = Column(Integer) #A/B
+    column = Column(Integer) #no
+    shelf = Column(Integer) #1-4
+    code = Column(String)
+
+
 class Readings(BASE):
-    __tablename__= 'readings'
+    """
+    Base class for the sensor Readings
+    """
 
-    ID = Column (Integer, primary_key=True, autoincrement=True)
-    Sensor_id =Column(Integer, ForeignKey ('sensor.id'))
-    sensor=relationship("Sensor", back_populates="READINGS")
-    TIME_CREATED = Column(DateTime(), server_default=func.now()) #when data are passed to the server
-    TIME_UPDATED = Column(DateTime(), onupdate=func.now()) #when data are passed in the sensor <-- to check
-    
-'''Class for reading the raw Advantix data'''
-class ReadingsAdvantix(BASE):
-    __tablename__= 'advantix'
+    __tablename__ = ADVANTIX_READINGS_TABLE_NAME
 
-    id = Column (Integer, primary_key=True, autoincrement=True)
-    Battery = Column(Float)
-    MODBUSID = Column (Integer)
-    TEMPERATURE = Column(Integer)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    sensor_id = Column(Integer, ForeignKey(SENSOR_TABLE_NAME+'.id'))
+    sensor_relationship = relationship(Sensor, back_populates="READINGS")
+    battery = Column(Float)
+    modbusid = Column(Integer)
+    temperature = Column(Integer)
+    time_created = Column(DateTime(), server_default=func.now()) #when data are passed to the server
+    time_updated = Column(DateTime(), onupdate=func.now()) #<-- to check
+
+
+class Readings_Advantix(BASE):
+    """
+    Class for reading the raw Advantix data
+    """
+    __tablename__ = "temp"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    battery = Column(Float)
+    modbusid = Column(Integer)
+    temperature = Column(Integer)
