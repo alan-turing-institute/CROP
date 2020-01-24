37ee7f7156b518226096947ef9a5e6c3a51fe564
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2020-01-24 15:49:54 +0000

    Function development locally

---
+++ utils/trigger_functions/advantixupload/advantixupload/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+import azure.functions as func
+
+
+def main(blobin: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {blobin.name}\n"
+                 f"Blob Size: {blobin.length} bytes")
