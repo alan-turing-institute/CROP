3dc86612719cfee793fa576bb17254ae9fb3a2cc
misspawty <flora.roumpani.11@ucl.ac.uk>
2020-02-04 12:43:04 +0000

    update on check database structure

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -14,48 +14,30 @@ from crop.sql import (
 
 from crop.constants import (
     SQL_DBNAME,
+    SQL_CONNECTION_STRING,
     CONST_TEST_DIR_DATA,
     CONST_ADVANTIX_FOLDER,
     CONST_ADVANTIX_TEST_1,
     CONST_ADVANTIX_TEST_2
 )
 
-from crop.ingress import advantix_read_csv
-
 from crop.create_db import create_database
 
 
 def test_db_creation ():
-    success, error = create_database(SQL_DBNAME)
-    assert(success == True)
-    assert(len(error) == 0)
+    error, log = create_database(SQL_CONNECTION_STRING, SQL_DBNAME)
+    assert error == False, log
+
 
 def test_db_connection ():
-    result, error= database_exists(SQL_CONNECTION_STRING_CROP), "database does not exist"
-    assert(result == False)
-    assert(len(error) > 0)
+    try: 
+        engine = create_engine(SQL_CONNECTION_STRING + SQL_DBNAME)
+    except:
+        error = True
+        log = "Error connecting to the database"
+        return error, log
 
 
-def test_advantix_SQL_insert():
-
-    # Check for non existing dataframe
-    data_df = None
-    result, error = advantix_SQL_insert(data_df, None, None, None, None, None)
-    assert(result == False)
-    assert(len(error) > 0)
-
-    # Check for empty dataframe
-    data_df = pd.DataFrame()
-    result, error = advantix_SQL_insert(data_df, None, None, None, None, None)
-    assert(result == False)
-    assert(len(error) > 0)
-
-    # Test wrong structure
-    file_path = os.path.join(CONST_TEST_DIR_DATA, CONST_ADVANTIX_FOLDER, CONST_ADVANTIX_TEST_2)
-    data_df = advantix_read_csv(file_path)
-    result, error = advantix_SQL_insert(data_df, None, None, None, None, None)
-    assert(result == False)
-    assert(len(error) > 0)
 
     
 
