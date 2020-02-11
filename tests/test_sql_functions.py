c6e1d5e59dc9a459d1bbd526c7699f8d6527b942
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2020-02-11 17:20:56 +0000

    Helping on with functionality

--- tests/test_sql_functions.py
+++ tests/test_sql_functions.py
@@ -1,39 +1,67 @@
 import pytest
 import os
 import sys
-from pathlib import Path
 import pandas as pd
-
-
-file = Path(__file__).resolve()
-parent, root = file.parent, file.parents[1]
-sys.path.append(str(root))
 
 from crop.populate_db import (
     bulk_insert_df
     )
 
-from crop.sql import (
-    advantix_SQL_insert
+from crop.create_db import (
+    create_database
 )
+
+from crop.sql_functions import {
+    check_sensor_exists
+}
+
+import crop.structure as structure
 
 from crop.constants import (
     SQL_DBNAME,
     SQL_CONNECTION_STRING,
     CONST_TEST_DIR_DATA,
     CONST_ADVANTIX_TEST_1,
-    ADVANTIX_READINGS_TABLE_NAME
+    ADVANTIX_READINGS_TABLE_NAME,
+    CONST_ADVANTIX_FOLDER
 )
 
+def test_check_sensor_exists():
+    
+    created, log = create_database(SQL_CONNECTION_STRING, SQL_DBNAME)
+    assert created, log
 
-def test_bulk_insert_df ():
+    # TODO: upload test data to the database
+    
 
-    conn_string = "{}{}".format(SQL_CONNECTION_STRING, SQL_DBNAME)
+    check_sensor_exists()
 
-    table_name_list = [ADVANTIX_READINGS_TABLE_NAME] #reads the advantix tablename from constants
-    for table_name in table_name_list:
-        exists, _ = check_table_exists(conn_string, table_name)
-        assert exists == True
+    # TODO: drop database
 
-def test_merge_df ():
-    Data= merge_df (engine, Data, Class)
+
+# def test_advantix_bulk_insert_df ():
+    
+#     created, log = create_database(SQL_CONNECTION_STRING, SQL_DBNAME)
+#     assert created, log
+
+#     conn_string = "{}{}".format(SQL_CONNECTION_STRING, SQL_DBNAME)
+
+#     table_name_list = [ADVANTIX_READINGS_TABLE_NAME] #reads the advantix tablename from constants
+#     for table_name in table_name_list:
+#         exists, _ = check_table_exists(conn_string, table_name)
+#         assert exists == True
+
+    # file_path = os.path.join(CONST_TEST_DIR_DATA, CONST_ADVANTIX_FOLDER, CONST_ADVANTIX_TEST_1)
+    # advantix_df = advantix_read_csv(file_path)
+
+ #   engine = create_engine(conn_string)
+
+
+
+    # bulk_insert_df(engine, advantix_df, structure.Readings_Advantix)
+
+
+    # TODO: drop database 
+
+# def test_merge_df ():
+#     Data = merge_df(engine, Data, Class)
