a818f44a2879e85d28a3fa7dae17d1a2d78672d3
misspawty <flora.roumpani.11@ucl.ac.uk>
2020-03-04 12:27:54 +0000

    small changes

--- crop/constants.py
+++ crop/constants.py
@@ -75,14 +75,14 @@ ERR_IMPORT_ERROR_4 = "Data contains duplicates"
 ERR_IMPORT_ERROR_5 = "Data contains invalid values"
 
 # Create connection string
-SQL_USER = os.environ["AZURE_SQL_USER"]
-SQL_PASSWORD = os.environ["AZURE_SQL_PASS"]
-SQL_SERVER = os.environ["AZURE_SQL_SERVER"]
-SQL_HOST = os.environ["AZURE_SQL_HOST"]
+SQL_USER = os.environ["CROP_SQL_USER"]
+SQL_PASSWORD = os.environ["CROP_SQL_PASS"]
+SQL_SERVER = os.environ["CROP_SQL_SERVER"]
+SQL_HOST = os.environ["CROP_SQL_HOST"]
 SQL_ENGINE = "postgresql"
-SQL_DBNAME = os.environ["AZURE_SQL_DBNAME"].strip().lower()
+SQL_DBNAME = os.environ["CROP_SQL_DBNAME"].strip().lower()
 SQL_DEFAULT_DBNAME = "postgres"
-SQL_PORT = os.environ["AZURE_SQL_PORT"]
+SQL_PORT = os.environ["CROP_SQL_PORT"]
 SQL_SSLMODE = "require"
 
 # FIXME: I am not quite sure, for this one seems to be messing up the connection
