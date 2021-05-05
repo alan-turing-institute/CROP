1da2a508dda28e2a37753d1695136f04a88abf7b
Yong May Yee <myong@turing.ac.uk>
2021-05-05 14:04:17 +0100

    Print dataframe

--- __app__/crop/ingress_weather.py
+++ __app__/crop/ingress_weather.py
@@ -14,7 +14,7 @@ from __app__.crop.db import connect_db, session_open, session_close
 from __app__.crop.structure import (
     TypeClass,
     SensorClass,
-    ReadingsZensieTRHClass,
+    ReadingsZensieWeatherClass,
 )
 from __app__.crop.utils import query_result_to_array
 from __app__.crop.constants import (
@@ -22,14 +22,14 @@ from __app__.crop.constants import (
     CONST_ZENSIE_WEATHER_SENSOR_TYPE
 )
 from __app__.crop.ingress import log_upload_event
-from __app__.crop.sensors import get_zensie_trh_sensor_data
+from __app__.crop.sensors import get_zensie_weather_sensor_data
 
 CONST_CHECK_URL_PATH = "https://api.30mhz.com/api/stats/check"
 CONST_CHECK_PARAMS = "statisticType=averages&intervalSize=60m"
 
 def import_zensie_data():
     """
-    Imports all zensie data
+    Imports all zensie weather data
 
     """
 
@@ -120,34 +120,36 @@ def import_zensie_weather_data(conn_string, database, dt_from, dt_to):
                 )
             )
 
-        #     if sensor_success:
-        #         # Sensor data from database
-        #         session = session_open(engine)
-        #         db_data_df = get_zensie_trh_sensor_data(
-        #             session,
-        #             sensor_id,
-        #             dt_from + timedelta(hours=-1),
-        #             dt_to + timedelta(hours=1),
-        #         )
-        #         session_close(session)
+            if sensor_success:
+                # Sensor data from database
+                session = session_open(engine)
+                db_data_df = get_zensie_weather_sensor_data(
+                  session,
+                  sensor_id,
+                  dt_from + timedelta(hours=-1),
+                  dt_to + timedelta(hours=1),
+                )
+                session_close(session)
 
-        #         if len(db_data_df) > 0:
-        #             # Filtering only new data
-        #             new_data_df = api_data_df[~api_data_df.index.isin(db_data_df.index)]
+                if len(db_data_df) > 0:
+                  # Filtering only new data
+                  new_data_df = api_data_df[~api_data_df.index.isin(db_data_df.index)]
 
-        #             logging.info(
-        #                 "sensor_id: {} | len(db_data_df): {}".format(
-        #                     sensor_id, len(db_data_df)
-        #                 )
-        #             )
-        #         else:
-        #             new_data_df = api_data_df
+                  logging.info(
+                      "sensor_id: {} | len(db_data_df): {}".format(
+                          sensor_id, len(db_data_df)
+                      )
+                  )
+                else:
+                  new_data_df = api_data_df
 
-        #         logging.info(
-        #             "sensor_id: {} | len(new_data_df): {}".format(
-        #                 sensor_id, len(new_data_df)
-        #             )
-        #         )
+                logging.info(
+                    "sensor_id: {} | len(new_data_df): {}".format(
+                        sensor_id, len(new_data_df)
+                    )
+                )
+
+                print (new_data_df)
 
         #         if len(new_data_df) > 0:
 
