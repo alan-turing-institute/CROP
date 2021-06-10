58a521701e49b2f63e4b008e5240f680fc7426b8
myyong <myong@turing.ac.uk>
2021-06-10 18:13:23 +0100

    Get and clean data

--- versioning/Data_model/test_model/arima/getData.R
+++ versioning/Data_model/test_model/arima/getData.R
@@ -1,5 +1,7 @@
 library(DBI)
 library(lubridate)
+require(reshape2)
+
 
 SECONDS.PERMINUTE = 60
 MINS.PERHOUR = 60
@@ -40,23 +42,7 @@ printTables = function(conn) {
   str(tables)
 }
 
-
-getData = function(numDays, limitRows) {
-  
-  datesToGetData = getStartEndDate(numDays)
-  
-  select_command = "SELECT sensors.name, zensie_trh_data.*"
-  from_command = "FROM sensor_types, sensors, zensie_trh_data"
-  where_criteria1 = "WHERE sensors.id = zensie_trh_data.sensor_id"
-  where_criteria2 = sprintf("AND zensie_trh_data.timestamp >= '%s'", datesToGetData$startDate)
-  where_criteria3 = sprintf("AND zensie_trh_data.timestamp < '%s'", datesToGetData$endDate)
-  
-  limit_command = ""
-  if (limitRows > 0)
-    limit_command = sprintf("LIMIT %i", limitRows)
-  sql_command = paste(select_command,from_command, where_criteria1, where_criteria2, where_criteria3, limit_command, sep=" ")
-  print (sql_command)
-  
+getData = function(sql_command) {
   # connect
   conn=connectToDatabase()
   
@@ -70,18 +56,58 @@ getData = function(numDays, limitRows) {
   data_result
 }
 
-getDataFromDatabase = function () {
-  env_raw = getData(numDays=30, limitRows=0)
-  env_raw$Timestamp2 <- as.POSIXct(env_raw$timestamp,tz="UTC")
+getTemperatureHumidityData = function(limitRows, datesToGetData) {
   
+  select_command = "SELECT sensors.name, zensie_trh_data.*"
+  from_command = "FROM sensor_types, sensors, zensie_trh_data"
+  where_criteria1 = "WHERE sensors.id = zensie_trh_data.sensor_id"
+  where_criteria2 = sprintf("AND zensie_trh_data.timestamp >= '%s'", datesToGetData$startDate)
+  where_criteria3 = sprintf("AND zensie_trh_data.timestamp < '%s'", datesToGetData$endDate)
+  
+  limit_command = ""
+  if (limitRows > 0)
+    limit_command = sprintf("LIMIT %i", limitRows)
+  sql_command = paste(select_command,from_command, where_criteria1, where_criteria2, where_criteria3, limit_command, sep=" ")
+
+  print(sql_command)
+  env_raw = getData(sql_command)
+  env_raw$Timestamp2 = as.POSIXct(env_raw$timestamp,tz="UTC")
   env_raw
+}
+
+getEnergyData = function (limitRows, datesToGetData) {
+  
+  #"""SELECT * FROM utc_energy_data WHERE utc_energy_data.timestamp >= '%s' AND utc_energy_data.timestamp < '%s'""" % (dt_from, dt_to)
+  select_command = "SELECT *"
+  from_command = "FROM utc_energy_data"
+  where_criteria1 = sprintf("WHERE utc_energy_data.timestamp >= '%s'", datesToGetData$startDate)
+  where_criteria2 = sprintf("AND utc_energy_data.timestamp < '%s'", datesToGetData$endDate)
+  
+  limit_command = ""
+  if (limitRows > 0)
+    limit_command = sprintf("LIMIT %i", limitRows)
+  sql_command = paste(select_command,from_command, where_criteria1, where_criteria2, limit_command, sep=" ")
+  
+  energy_raw = getData(sql_command)
+  energy_raw$Timestamp2 <- as.POSIXct(energy_raw$timestamp,tz="UTC")
+  energy_raw
 }
 
 getDataFromCsv = function() {
   env_raw = read.csv("./data/test10.csv")
 }
 
-env_raw = getDataFromCsv()
+numDays = 40
+limitRows = 0
+datesToGetData = getStartEndDate(numDays)
+
+#energy_raw = getEnergyData(limitRows = limitRows, datesToGetData = datesToGetData)
+#write.csv(energy_raw, "./data/energy40.csv")
+#energy_raw = read.csv("./data/energy60.csv")
+
+env_raw = getTemperatureHumidityData(limitRows = limitRows, datesToGetData = datesToGetData)
+write.csv(env_raw, "./data/env40.csv")
+#env_raw = read.csv("./data/env40.csv")
 
 #source(paste0(".","/cleandata.R"), echo=FALSE)
 
