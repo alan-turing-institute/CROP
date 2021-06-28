13ae2f3ef611cbe124e6d4dde4062df40fc4acb9
misspawty <flora.roumpani.11@ucl.ac.uk>
2021-06-28 16:01:27 +0100

    stacked bars

--- webapp/app/home/routes.py
+++ webapp/app/home/routes.py
@@ -3,9 +3,8 @@ from flask import render_template
 from flask_login import login_required
 
 
-@blueprint.route('/<template>')
+@blueprint.route("/<template>")
 @login_required
 def route_template(template):
-    
 
-    return render_template(template + '.html')
+    return render_template(template + ".html")
