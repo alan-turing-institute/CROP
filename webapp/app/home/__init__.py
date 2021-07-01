37ae5f4c27ac287e7a91045b7d4cc6832a88fcc3
misspawty <flora.roumpani.11@ucl.ac.uk>
2021-07-01 20:34:42 +0100

    Update __init__.py

--- webapp/app/home/__init__.py
+++ webapp/app/home/__init__.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 blueprint = Blueprint(
     "home_blueprint",
     __name__,
-    url_prefix="/home1",
+    url_prefix="/home",
     template_folder="templates",
     static_folder="static",
 )
