e1edbe9fa09472b83e8d737ba09c3fbc281619a6
myyong <myong@turing.ac.uk>
2021-07-06 17:33:24 +0100

    Mock data

--- utils/create_azure_infrastructure/scripts/create_predictionTable.sql
+++ utils/create_azure_infrastructure/scripts/create_predictionTable.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS model;
-DROP TABLE IF EXISTS measure;
-DROP TABLE IF EXISTS prediction;
+DROP TABLE IF EXISTS model_measure;
+DROP TABLE IF EXISTS model_prediction;
+DROP TABLE IF EXISTS model_run;
 
--- Model:
+-- --Model:
 CREATE TABLE model (
   id SERIAL PRIMARY KEY,
   model_name VARCHAR(100) NOT NULL,
@@ -10,33 +11,40 @@ CREATE TABLE model (
 );
 
 INSERT INTO model(model_name, author)
-VALUES('arima', 'Melanie Singh')
+VALUES('arima', 'Melanie Singh');
 
 -- Measure:
-CREATE TABLE measure (
+CREATE TABLE model_measure (
   id SERIAL PRIMARY KEY,
   measure_name VARCHAR(100) NOT NULL,
-  description VARCHAR(100)
+  measure_description VARCHAR(100)
 );
 
-INSERT INTO measure(measure_name)
-VALUES('Mean Temperature (Degree Celcius)')
+INSERT INTO model_measure(measure_name)
+VALUES('Mean Temperature (Degree Celcius)');
 
-INSERT INTO measure(measure_name)
-VALUES('Upper Bound Temperature (Degree Celcius)')
+INSERT INTO model_measure(measure_name)
+VALUES('Upper Bound Temperature (Degree Celcius)');
 
-INSERT INTO measure(measure_name)
-VALUES('Lower Bound Temperature (Degree Celcius)')
+INSERT INTO model_measure(measure_name)
+VALUES('Lower Bound Temperature (Degree Celcius)');
 
-CREATE TABLE run_output (
+-- Record run:
+CREATE TABLE model_run (
   id SERIAL PRIMARY KEY,
   sensor_id INTEGER NOT NULL,
   measure_id INTEGER NOT NULL,
-  timestamp timestamp default current_timestamp NOT NULL
+  time_created timestamp default current_timestamp NOT NULL
 );
 
-CREATE TABLE prediction (
+INSERT INTO model_run(sensor_id, measure_id)
+VALUES(27, 0);
+
+-- Prdiction value:
+CREATE TABLE model_prediction (
   id SERIAL PRIMARY KEY,
-  prediction_value FLOAT NOT NULL,
-  unit VARCHAR(100) NOT NULL
+  prediction_value FLOAT NOT NULL
 );
+
+INSERT INTO model_prediction(prediction_value)
+VALUES(15);
