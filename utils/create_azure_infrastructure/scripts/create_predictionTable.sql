c5c35aad467a8973ad1b309c908939a29725d9bd
myyong <myong@turing.ac.uk>
2021-07-08 17:27:49 +0100

    Scripts for creating tables for supporting prediction records

--- utils/create_azure_infrastructure/scripts/create_predictionTable.sql
+++ utils/create_azure_infrastructure/scripts/create_predictionTable.sql
@@ -1,50 +1,70 @@
-DROP TABLE IF EXISTS model;
-DROP TABLE IF EXISTS model_measure;
-DROP TABLE IF EXISTS model_prediction;
-DROP TABLE IF EXISTS model_run;
+-- DROP TABLE IF EXISTS model CASCADE;
+-- DROP TABLE IF EXISTS model_measure CASCADE;
+DROP TABLE IF EXISTS model_run CASCADE;
+DROP TABLE IF EXISTS model_prediction CASCADE;
 
 -- --Model:
-CREATE TABLE model (
-  id SERIAL PRIMARY KEY,
-  model_name VARCHAR(100) NOT NULL,
-  author VARCHAR(100)
-);
+-- CREATE TABLE model (
+--   id SERIAL PRIMARY KEY,
+--   model_name VARCHAR(100) NOT NULL,
+--   author VARCHAR(100)
+-- );
 
-INSERT INTO model(model_name, author)
-VALUES('arima', 'Melanie Singh');
+-- INSERT INTO model(model_name, author)
+-- VALUES('arima', 'Melanie Singh');
 
--- Measure:
-CREATE TABLE model_measure (
-  id SERIAL PRIMARY KEY,
-  measure_name VARCHAR(100) NOT NULL,
-  measure_description VARCHAR(100)
-);
+-- -- Measure:
+-- CREATE TABLE model_measure (
+--   id SERIAL PRIMARY KEY,
+--   measure_name VARCHAR(100) NOT NULL,
+--   measure_description VARCHAR(100)
+-- );
 
-INSERT INTO model_measure(measure_name)
-VALUES('Mean Temperature (Degree Celcius)');
+-- INSERT INTO model_measure(measure_name)
+-- VALUES('Mean Temperature (Degree Celcius)');
 
-INSERT INTO model_measure(measure_name)
-VALUES('Upper Bound Temperature (Degree Celcius)');
+-- INSERT INTO model_measure(measure_name)
+-- VALUES('Upper Bound Temperature (Degree Celcius)');
 
-INSERT INTO model_measure(measure_name)
-VALUES('Lower Bound Temperature (Degree Celcius)');
+-- INSERT INTO model_measure(measure_name)
+-- VALUES('Lower Bound Temperature (Degree Celcius)');
 
 -- Record run:
 CREATE TABLE model_run (
   id SERIAL PRIMARY KEY,
-  sensor_id INTEGER NOT NULL,
-  measure_id INTEGER NOT NULL,
+  sensor_id INTEGER REFERENCES sensors(id),
+  model_id INTEGER REFERENCES model(id),
   time_created timestamp default current_timestamp NOT NULL
 );
 
-INSERT INTO model_run(sensor_id, measure_id)
-VALUES(27, 0);
-
--- Prdiction value:
-CREATE TABLE model_prediction (
+CREATE TABLE model_product (
   id SERIAL PRIMARY KEY,
-  prediction_value FLOAT NOT NULL
+  run_id INTEGER REFERENCES model_run(id),
+  measure_id INTEGER REFERENCES model_measure(id)
 );
 
-INSERT INTO model_prediction(prediction_value)
-VALUES(15);
+
+-- INSERT INTO model_run(sensor_id, measure_id, model_id)
+-- VALUES(18, 1, 1);
+
+-- Prdiction value:
+CREATE TABLE model_value (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER REFERENCES model_product(id),
+  prediction_value FLOAT NOT NULL,
+  prediction_index INTEGER NOT NULL
+);
+
+-- INSERT INTO model_prediction(run_id,prediction_value, prediction_index)
+-- VALUES(1, 11, 1),
+-- (1, 12, 2),
+-- (1, 13, 3),
+-- (1, 14, 4),
+-- (1, 15, 5),
+-- (1, 16, 6),
+-- (1, 17, 7),
+-- (1, 18, 8),
+-- (1, 19, 9),
+-- (1, 20, 10),
+-- (1, 21, 11),
+-- (1, 22, 12);
---
+++ utils/create_azure_infrastructure/scripts/deleteRun.sql
@@ -0,0 +1,3 @@
+delete from model_value;
+delete from model_product;
+delete from model_run;
