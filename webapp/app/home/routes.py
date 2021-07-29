689ce61b65b2f245ead10da05bbf101e626f8e9c
misspawty <flora.roumpani.11@ucl.ac.uk>
2021-07-29 23:44:20 +0100

    vertical stratification backend

--- webapp/app/home/routes.py
+++ webapp/app/home/routes.py
@@ -23,7 +23,7 @@ from __app__.crop.structure import (
     SensorLocationClass,
     ReadingsZensieTRHClass,
 )
-from __app__.crop.constants import CONST_MAX_RECORDS, CONST_TIMESTAMP_FORMAT
+from __app__.crop.constants import CONST_TIMESTAMP_FORMAT
 
 TEMP_BINS = {
     "Propagation": [0.0, 20.0, 23.0, 25.0, 35.0],  # optimal 23
@@ -136,16 +136,15 @@ def zensie_query(
     logging.info("Total number of records found: %d" % (len(df.index)))
 
     if not df.empty:
-        df_mean_hr = mean_sensor_values_per_hr(df)
+        return df
 
     else:
-        location_ids = []
-        sensor_temp_ranges = {}
+        df_mean_hr = df.empty
 
-    return df_mean_hr
+    return df
 
 
-def mean_sensor_values_per_hr(temp_df):
+def grp_per_hr_zone(temp_df, dt_from, dt_to):
     """
     finds mean of values per hour for a selected date.
     Returns a dataframe ready for json
@@ -161,16 +160,20 @@ def mean_sensor_values_per_hr(temp_df):
 
     df = copy.deepcopy(temp_df)
 
+    # mask per selected date
+    mask = (df["timestamp"] >= dt_from) & (df["timestamp"] <= dt_to)
+    filtered_df = df.loc[mask]
+
     # extracting date from datetime
-    df["date"] = pd.to_datetime(df["timestamp"].dt.date)
+    filtered_df["date"] = pd.to_datetime(filtered_df["timestamp"].dt.date)
 
     # Reseting index
-    df.sort_values(by=["timestamp"], ascending=True).reset_index(inplace=True)
+    filtered_df.sort_values(by=["timestamp"], ascending=True).reset_index(inplace=True)
 
     df_grp_zone_hr = (
-        df.groupby(
+        filtered_df.groupby(
             by=[
-                df.timestamp.map(
+                filtered_df.timestamp.map(
                     lambda x: "%04d-%02d-%02d-%02d" % (x.year, x.month, x.day, x.hour)
                 ),
                 "zone",
@@ -184,7 +187,51 @@ def mean_sensor_values_per_hr(temp_df):
     return df_grp_zone_hr
 
 
+def vertical_stratification(temp_df, bot_sensor_id, top_sensor_id, dt_from, dt_to):
+
+    df = copy.deepcopy(temp_df)
+
+    # mask per selected date
+    mask = (df["timestamp"] >= dt_from) & (df["timestamp"] <= dt_to)
+    filtered_df = df.loc[mask]
+
+    # extracting date from datetime
+    filtered_df["date"] = pd.to_datetime(filtered_df["timestamp"].dt.date)
+
+    # Reseting index
+    filtered_df.sort_values(by=["timestamp"], ascending=True).reset_index(inplace=True)
+
+    df_ = filtered_df.loc[
+        (df["sensor_id"] == bot_sensor_id) | (df["sensor_id"] == top_sensor_id)
+    ]
+
+    df_grp_hr = (
+        df_.groupby(
+            by=[
+                df_.timestamp.map(
+                    lambda x: "%04d-%02d-%02d-%02d" % (x.year, x.month, x.day, x.hour)
+                ),
+                "date",
+                "sensor_id",
+            ]
+        )
+        .mean()
+        .reset_index()
+    )
+
+    json_VS = (
+        df_grp_hr.groupby(["sensor_id"], as_index=True)
+        .apply(lambda x: x[["temperature", "humidity"]].to_dict("r"))
+        .reset_index()
+        .rename(columns={0: "Values"})
+        .to_json(orient="records")
+    )
+
+    return df_grp_hr
+
+
 def temperature_analysis(df, dt_from, dt_to, bins):
+    # print(df["timestamp"])
 
     df_unique_zones = df[["zone"]].drop_duplicates(["zone"])
     location_zones = df_unique_zones["zone"].tolist()
@@ -336,15 +383,29 @@ def route_template(template):
 
     dt_to = dt.datetime.now()
     dt_from = dt_to - dt.timedelta(days=7)
-    df_mean = zensie_query(dt_from, dt_to)
-    df_temp = temperature_analysis(df_mean, dt_from, dt_to, TEMP_BINS)
-    df_hum = humidity_analysis(df_mean, dt_from, dt_to, HUM_BINS)
+    dt_from_daily = dt_to - dt.timedelta(days=1)
 
-    weekly_temp_json = Prepare_Json_temp(df_temp)
-    weekly_hum_json = Prepare_Json_temp(df_hum)
+    df = zensie_query(dt_from, dt_to)
+
+    df_mean_hr_weekly = grp_per_hr_zone(df, dt_from, dt_to)
+    df_mean_hr_daily = grp_per_hr_zone(df, dt_from_daily, dt_to)
+
+    df_temp_weekly = temperature_analysis(df_mean_hr_weekly, dt_from, dt_to, TEMP_BINS)
+    df_hum_weekly = humidity_analysis(df_mean_hr_weekly, dt_from, dt_to, HUM_BINS)
+    df_temp_daily = temperature_analysis(
+        df_mean_hr_daily, dt_from_daily, dt_to, TEMP_BINS
+    )
+    df_hum_daily = humidity_analysis(df_mean_hr_daily, dt_from_daily, dt_to, HUM_BINS)
+
+    weekly_temp_json = Prepare_Json_temp(df_temp_weekly)
+    weekly_hum_json = Prepare_Json_temp(df_hum_weekly)
+    daily_temp_json = Prepare_Json_temp(df_temp_daily)
+    daily_hum_json = Prepare_Json_temp(df_hum_daily)
 
     # data_to_frontend = weekly_temp_json # for the end merged json
-
+    vertical_stratification(
+        df, 23, 18, dt_from, dt_to
+    )  # sensorids in positions (16B1 and 16B4)
     # print(data_to_frontend)
 
     a = "test!!"
@@ -354,6 +415,8 @@ def route_template(template):
             template + ".html",
             temperature_data=weekly_temp_json,
             humidity_data=weekly_hum_json,
+            temperature_data_daily=daily_temp_json,
+            humidity_data_daily=daily_hum_json,
             dt_from=dt_from.strftime("%B %d, %Y"),
             dt_to=dt_to.strftime("%B %d, %Y"),
         )
