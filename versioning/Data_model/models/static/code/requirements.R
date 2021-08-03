c98dc36ea26a3109848145baa8a6c2c3d8097c6c
myyong <myong@turing.ac.uk>
2021-08-03 14:51:46 +0100

    Dockerfile for setting up RPostgreSQL-enabled environment

--- versioning/Data_model/models/static/code/requirements.R
+++ versioning/Data_model/models/static/code/requirements.R
@@ -3,12 +3,12 @@
 # install.packages("RPostgreSQL_0.6-2.tgz")
 
 install.packages("DBI")
-install.packages("postgresql-devel")
+install.packages("RPostgreSQL")
 
-install.packages("RPostgreSQL_0.6-2.tgz")
+# install.packages("RPostgreSQL_0.6-2.tgz")
 
 #install.packages(file.choose(), repos = NULL)
-# install.packages("lubridate")
+install.packages("lubridate")
 # install.packages("reshape2")
 
 # install.packages("plyr")
