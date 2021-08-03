a4c5c9c4db8065b0bba1695865c9e7469c1cc8ad
myyong <myong@turing.ac.uk>
2021-08-03 14:51:46 +0100

    Dockerfile for setting up RPostgreSQL-enabled environment

--- versioning/Data_model/models/static/code/test_connection.R
+++ versioning/Data_model/models/static/code/test_connection.R
@@ -7,7 +7,7 @@ getConnection = function(){
   crop_port = "5432"
   crop_dbname = "app_db"
   crop_user = "cropdbadmin@cropapptestsqlserver"
-  crop_password = ""
+  crop_password = "QhXZ7qZddDr224Mc2P4k"
   
   # Connect to the MySQL database: con
   con <- DBI::dbConnect(RPostgreSQL::PostgreSQL(), 
