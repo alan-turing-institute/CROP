b68fe106e453b2329b91c6cda21a2d01bda664ef
myyong <myong@turing.ac.uk>
2021-08-03 11:12:03 +0100

    New data structure

---
+++ versioning/Data_model/models/static/code/requirements.R
@@ -0,0 +1,19 @@
+
+# install.packages("DBI")
+# install.packages("RPostgreSQL_0.6-2.tgz")
+
+install.packages("DBI")
+install.packages("postgresql-devel")
+
+install.packages("RPostgreSQL_0.6-2.tgz")
+
+#install.packages(file.choose(), repos = NULL)
+# install.packages("lubridate")
+# install.packages("reshape2")
+
+# install.packages("plyr")
+# install.packages("ggplot2")
+# 
+# install.packages("bsts")
+# install.packages("dplyr")
+# install.packages("forecast")
