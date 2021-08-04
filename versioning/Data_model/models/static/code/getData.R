70326741626bd457fde8f760bf10db21807fdcd2
myyong <myong@turing.ac.uk>
2021-08-04 15:51:35 +0100

    Clear up data access

--- versioning/Data_model/models/static/code/getData.R
+++ versioning/Data_model/models/static/code/getData.R
@@ -18,7 +18,7 @@ connectToDatabase = function(){
   crop_port = "5432"
   crop_dbname = "app_db"
   crop_user = "cropdbadmin@cropapptestsqlserver"
-  crop_password = "QhXZ7qZddDr224Mc2P4k"
+  crop_password = ""
   
   # Connect to the MySQL database: con
   con <- DBI::dbConnect(RPostgreSQL::PostgreSQL(), 
