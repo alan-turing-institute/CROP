1016663ce225f4d001b629f07b140e58702fdd3f
myyong <myong@turing.ac.uk>
2021-09-03 15:23:48 +0100

    Code for adding particles history

--- utils/create_azure_infrastructure/scripts/create_predictionTable.sql
+++ utils/create_azure_infrastructure/scripts/create_predictionTable.sql
@@ -75,13 +75,6 @@ CREATE TABLE model_value (
 -- (1, 21, 11),
 -- (1, 22, 12);
 
-CREATE TABLE model_parameter (
-  id SERIAL PRIMARY KEY,
-  parameter_id INTEGER REFERENCES parameter(id),
-  parameter_index INTEGER NOT NULL,
-  parameter_value INTEGER NOT NULL
-);
-
 CREATE TABLE parameter (
   id SERIAL PRIMARY KEY,
   model_id INTEGER REFERENCES model(id),
@@ -92,3 +85,18 @@ INSERT INTO parameter(model_id, parameter_name)
 VALUES(2, 'length_out'),
 (2, 'ACH'),
 (2, 'IAS');
+
+CREATE TABLE model_parameter (
+  id SERIAL PRIMARY KEY,
+  time_created timestamp default current_timestamp NOT NULL,
+  parameter_id INTEGER REFERENCES parameter(id),
+  parameter_index INTEGER NOT NULL,
+  parameter_value FLOAT NOT NULL
+);
+
+INSERT INTO model_parameter(parameter_id, parameter_value)
+VALUES (2, 0, 0.4594613726254301), 
+(2, 1, 0.763604572422916), 
+(2, 2, 0.7340651592924317), 
+(2, 3, 0.7047730309779485), 
+(2, 4, 0.4595117250921914)
