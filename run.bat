b3c5be561855e5259a80a42076918b5abfbb1dda
Flora Roumpani <froumpani@turing.ac.uk>
2022-01-26 01:56:26 +0000

    update submodule

--- run.bat
+++ run.bat
@@ -4,5 +4,6 @@ title local server from conda
 echo runing the webapp
 call "C:\ProgramData\Anaconda3\Scripts\activate.bat" activate crop
 cd "C:\Users\froumpani\OneDrive - The Alan Turing Institute\Turing\1.Projects\2.UrbanAgriculture\CROP\webapp"
+git submodule update --remote
 set FLASK_ENV=development
 flask run --host=0.0.0.0 --port 5000
