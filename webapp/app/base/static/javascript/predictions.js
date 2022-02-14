9d3e89139163109a4ee6b65745306f15c5cb2c0e
Markus Hauru <mhauru@turing.ac.uk>
2022-02-14 14:51:30 +0000

    Add front/back plot, improve style

--- webapp/app/base/static/javascript/predictions.js
+++ webapp/app/base/static/javascript/predictions.js
@@ -1,22 +1,3 @@
-// Zip to arrays [x0, x1, x2, ...] and [y0, y1, y2, ...] into
-// [{x: x0, y: 01}, {x: x1, y: y1}, {x: x2, y: y2}, ...]
-// Used to gather data for plots into a format preferred by Chart.js
-function dictionary_scatter(x , y) {
-  value_array = []
-  for (var j=0; j < y.length; j++) {
-    const mydict = {x: x[j], y: y[j]}
-    value_array.push(mydict)
-  }
-  return value_array
-}
-
-
-// Sort an array of objects by the field elname, which each object is assumed
-// to have, and which is assumed to be a number. Operates in place.
-function sort_by_numerical(arr, elname) {
-  return arr.sort((e1, e2) => e1[elname] - e2[elname])
-}
-
 function plot(top_json, mid_json, bot_json, sce_json, zensie_json, zensie_measure, canvasname, y_label, show_legend, scenario_name) {
   sort_by_numerical(top_json["Values"], "prediction_index")
   sort_by_numerical(mid_json["Values"], "prediction_index")
---
+++ webapp/app/base/static/javascript/utility.js
@@ -0,0 +1,18 @@
+// Zip to arrays [x0, x1, x2, ...] and [y0, y1, y2, ...] into
+// [{x: x0, y: 01}, {x: x1, y: y1}, {x: x2, y: y2}, ...]
+// Used to gather data for plots into a format preferred by Chart.js
+function dictionary_scatter(x , y) {
+  value_array = []
+  for (var j=0; j < y.length; j++) {
+    const mydict = {x: x[j], y: y[j]}
+    value_array.push(mydict)
+  }
+  return value_array
+}
+
+
+// Sort an array of objects by the field elname, which each object is assumed
+// to have, and which is assumed to be a number. Operates in place.
+function sort_by_numerical(arr, elname) {
+  return arr.sort((e1, e2) => e1[elname] - e2[elname])
+}
