8948cdde1a665e6d7080ffd1d82636c9c2e1c338
Markus Hauru <mhauru@turing.ac.uk>
2022-04-13 10:33:56 +0100

    Clean up root directory files

--- run.bat
+++
@@ -1,9 +0,0 @@
-title local server from conda
-//conda create --name crop python=3.8
-//pip3 install -r./requirements.txt
-echo runing the webapp
-call "C:\ProgramData\Anaconda3\Scripts\activate.bat" activate crop
-cd "C:\Users\froumpani\OneDrive - The Alan Turing Institute\Turing\1.Projects\2.UrbanAgriculture\CROP\webapp"
-git submodule update --remote
-set FLASK_ENV=development
-flask run --host=0.0.0.0 --port 5000
