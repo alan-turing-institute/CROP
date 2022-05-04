d4d8c344b7610068d04a40ca4ac6f85584e440ef
Markus Hauru <mhauru@turing.ac.uk>
2022-05-04 17:02:15 +0100

    Print more errors in zensie_func

--- __app__/crop/ingress_zensie.py
+++ __app__/crop/ingress_zensie.py
@@ -151,10 +151,12 @@ def import_zensie_trh_data(conn_string, database, dt_from, dt_to):
             success = False
             log = "No sensors with sensor type {} were found.".format(sensor_type)
 
-    except:
+    except Exception as e:
         session_close(session)
         success = False
         log = "No sensors with sensor type {} were found.".format(sensor_type)
+        error_msg = "Caught exception: {}".format(e)
+        logging.error(error_msg)
 
     if not success:
         logging.info(log)
