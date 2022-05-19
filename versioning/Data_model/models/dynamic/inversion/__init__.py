43e57592fe5ba9ddf94ce687dc4bae8292235a14
Markus Hauru <mhauru@turing.ac.uk>
2022-05-19 14:39:30 +0100

    Add inversion back in as part of the repo

---
+++ versioning/Data_model/models/dynamic/inversion/__init__.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import numpy as np
+import math
+import matplotlib.pyplot as plot
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+from scipy.stats import norm
+import inversion.utils as utils
+import inversion.calibration as calibration
+import warnings
+import inversion.kernels as kernels
+from sklearn.exceptions import ConvergenceWarning
+
+warnings.filterwarnings(action="ignore", category=ConvergenceWarning)
