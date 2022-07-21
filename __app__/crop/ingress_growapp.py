41cbbf14530fd883ad612887f990beee78da20d7
nbarlowATI <nbarlow@turing.ac.uk>
2022-07-21 09:54:40 +0100

    WIP starting work on script to incress growapp data

--- __app__/crop/ingress_growapp.py
+++ __app__/crop/ingress_growapp.py
@@ -6,6 +6,7 @@ import time
 from datetime import datetime, timedelta
 import requests
 import pandas as pd
+from urllib import parse
 
 from sqlalchemy import and_
 from sqlalchemy.exc import ProgrammingError
@@ -33,6 +34,56 @@ from __app__.crop.constants import (
     GROWAPP_USER,
     GROWAPP_PASSWORD,
     GROWAPP_SCHEMA,
+    SQL_CONNECTION_STRING,
+    SQL_DBNAME,
+    SQL_ENGINE,
 )
 
 from __app__.crop.ingress import log_upload_event
+
+
+def get_growapp_db_session():
+    """
+    Get an SQLAlchemy session on the GrowApp database.
+
+    Returns
+    =======
+    session: SQLAlchemy session object
+    """
+    conn_string = "%s://%s:%s@%s" % (
+        SQL_ENGINE,
+        GROWAPP_USER,
+        parse.quote(GROWAPP_PASSWORD),
+        GROWAPP_IP,
+    )
+    success, log, engine = connect_db(conn_string, GROWAPP_DB)
+    if not success:
+        logging.info(log)
+        return None
+    session = session_open(engine)
+    return session
+
+def get_cropapp_db_session():
+    """
+    Get an SQLAlchemy session on the CROP database.
+
+    Returns
+    =======
+    session: SQLAlchemy session object
+    """
+    success, log, engine = connect_db(SQL_CONNECTION_STRING, SQL_DBNAME)
+    if not success:
+        logging.info(log)
+        return None
+    session = session_open(engine)
+    return session
+
+
+def read_crop_type_data():
+    """
+    Read from the table of crops in the GrowApp database
+
+    Returns
+    =======
+    crop_data: list of dicts
+    """
