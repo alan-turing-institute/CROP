19efda4a682b869520c0670c64aa65f4c38397a4
nbarlowATI <nbarlow@turing.ac.uk>
2022-08-03 12:07:18 +0100

    dummy commit to __init__ to test function

--- versioning/Data_model/models/dynamic/ges_func/__init__.py
+++ versioning/Data_model/models/dynamic/ges_func/__init__.py
@@ -6,8 +6,8 @@ import azure.functions as func
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    # The order of these imports, that GESCalibrationV1 is run before pipelineV1_1 is
-    # imported, actually matters.
+    # The order of these imports, that GESCalibrationV1.main() is run
+    # before pipelineV1_1 is imported, actually matters.
     import GESCalibrationV1
 
     GESCalibrationV1.main()
