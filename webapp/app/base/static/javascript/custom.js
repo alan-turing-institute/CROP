b0b9e1930e4d22840e3d5957f8cdb342bdb7f1a9
Markus Hauru <mhauru@turing.ac.uk>
2022-08-24 10:14:21 +0100

    Remove vendors/switchery and its imports

--- webapp/app/base/static/javascript/custom.js
+++ webapp/app/base/static/javascript/custom.js
@@ -205,21 +205,6 @@ if ($(".progress .progress-bar")[0]) {
 }
 // /Progressbar
 
-// Switchery
-$(document).ready(function () {
-  if ($(".js-switch")[0]) {
-    var elems = Array.prototype.slice.call(
-      document.querySelectorAll(".js-switch")
-    );
-    elems.forEach(function (html) {
-      var switchery = new Switchery(html, {
-        color: "#26B99A",
-      });
-    });
-  }
-});
-// /Switchery
-
 // iCheck
 $(document).ready(function () {
   if ($("input.flat")[0]) {
