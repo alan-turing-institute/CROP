ad3c3f879035d7788d153d0b25ea95b95b5b0573
Markus Hauru <mhauru@turing.ac.uk>
2022-08-24 14:35:27 +0100

    Remove dead javascript (nprogress)

--- webapp/app/base/static/javascript/custom.js
+++ webapp/app/base/static/javascript/custom.js
@@ -210,17 +210,6 @@ $(document).ready(function () {
   });
 });
 
-// NProgress
-if (typeof NProgress != "undefined") {
-  $(document).ready(function () {
-    NProgress.start();
-  });
-
-  $(window).load(function () {
-    NProgress.done();
-  });
-}
-
 //hover and retain popover when on popover content
 var originalLeave = $.fn.popover.Constructor.prototype.leave;
 $.fn.popover.Constructor.prototype.leave = function (obj) {
