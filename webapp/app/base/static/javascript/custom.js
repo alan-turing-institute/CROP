8e4d0439d0a18bff7a6ce03526089904751c5f73
Markus Hauru <mhauru@turing.ac.uk>
2022-08-24 10:19:53 +0100

    Remove vendors/skycons and its imports

--- webapp/app/base/static/javascript/custom.js
+++ webapp/app/base/static/javascript/custom.js
@@ -641,34 +641,6 @@ function init_JQVmap() {
   }
 }
 
-function init_skycons() {
-  if (typeof Skycons === "undefined") {
-    return;
-  }
-  console.log("init_skycons");
-
-  var icons = new Skycons({
-      color: "#73879C",
-    }),
-    list = [
-      "clear-day",
-      "clear-night",
-      "partly-cloudy-day",
-      "partly-cloudy-night",
-      "cloudy",
-      "rain",
-      "sleet",
-      "snow",
-      "wind",
-      "fog",
-    ],
-    i;
-
-  for (i = list.length; i--; ) icons.set(list[i], list[i]);
-
-  icons.play();
-}
-
 function init_chart_doughnut() {
   if (typeof Chart === "undefined") {
     return;
@@ -5843,14 +5815,12 @@ $(document).ready(function () {
   init_charts();
   init_echarts();
   init_morris_charts();
-  init_skycons();
   init_select2();
   init_validator();
   init_DataTables();
   init_chart_doughnut();
   init_gauge();
   init_PNotify();
-  init_starrr();
   init_calendar();
   init_compose();
   init_CustomNotification();
