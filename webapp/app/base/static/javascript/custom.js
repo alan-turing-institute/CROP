5f192a11ece43016bbe4c8173a55e3cf9c8d8eb6
Markus Hauru <mhauru@turing.ac.uk>
2022-08-24 11:01:00 +0100

    Remove vendors/nprogress and its imports

--- webapp/app/base/static/javascript/custom.js
+++ webapp/app/base/static/javascript/custom.js
@@ -1241,26 +1241,6 @@ function init_TagsInput() {
   }
 }
 
-/* SELECT2 */
-
-function init_select2() {
-  if (typeof select2 === "undefined") {
-    return;
-  }
-  console.log("init_toolbox");
-
-  $(".select2_single").select2({
-    placeholder: "Select a state",
-    allowClear: true,
-  });
-  $(".select2_group").select2({});
-  $(".select2_multiple").select2({
-    maximumSelectionLength: 4,
-    placeholder: "With Max Selection limit 4",
-    allowClear: true,
-  });
-}
-
 /* WYSIWYG EDITOR */
 
 function init_wysiwyg() {
@@ -5815,7 +5795,6 @@ $(document).ready(function () {
   init_charts();
   init_echarts();
   init_morris_charts();
-  init_select2();
   init_validator();
   init_DataTables();
   init_chart_doughnut();
