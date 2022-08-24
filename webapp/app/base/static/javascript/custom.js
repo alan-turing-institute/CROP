33b1aab98ae0ffe14bfa5e924c10f4f5b5073728
Markus Hauru <mhauru@turing.ac.uk>
2022-08-24 11:10:10 +0100

    Remove vendors/malihu-custom-scrollbar-plugin and its imports

--- webapp/app/base/static/javascript/custom.js
+++ webapp/app/base/static/javascript/custom.js
@@ -147,15 +147,6 @@ function init_sidebar() {
   });
 
   setContentHeight();
-
-  // fixed sidebar
-  if ($.fn.mCustomScrollbar) {
-    $(".menu_fixed").mCustomScrollbar({
-      autoHideScrollbar: true,
-      theme: "minimal",
-      mouseWheel: { preventDefault: true },
-    });
-  }
 }
 // /Sidebar
 
