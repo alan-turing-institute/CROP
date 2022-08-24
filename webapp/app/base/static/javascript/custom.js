93a81e3fd7734c95e5300e391188b3fc5a8bf72d
Markus Hauru <mhauru@turing.ac.uk>
2022-08-24 13:12:59 +0100

    Remove some remnants of the vendors clean-up

--- webapp/app/base/static/javascript/custom.js
+++ webapp/app/base/static/javascript/custom.js
@@ -487,117 +487,6 @@ function init_parsley() {
   } catch (err) {}
 }
 
-/* WYSIWYG EDITOR */
-
-function init_wysiwyg() {
-  if (typeof $.fn.wysiwyg === "undefined") {
-    return;
-  }
-  console.log("init_wysiwyg");
-
-  function init_ToolbarBootstrapBindings() {
-    var fonts = [
-        "Serif",
-        "Sans",
-        "Arial",
-        "Arial Black",
-        "Courier",
-        "Courier New",
-        "Comic Sans MS",
-        "Helvetica",
-        "Impact",
-        "Lucida Grande",
-        "Lucida Sans",
-        "Tahoma",
-        "Times",
-        "Times New Roman",
-        "Verdana",
-      ],
-      fontTarget = $("[title=Font]").siblings(".dropdown-menu");
-    $.each(fonts, function (idx, fontName) {
-      fontTarget.append(
-        $(
-          '<li><a data-edit="fontName ' +
-            fontName +
-            '" style="font-family:\'' +
-            fontName +
-            "'\">" +
-            fontName +
-            "</a></li>"
-        )
-      );
-    });
-    $("a[title]").tooltip({
-      container: "body",
-    });
-    $(".dropdown-menu input")
-      .click(function () {
-        return false;
-      })
-      .change(function () {
-        $(this)
-          .parent(".dropdown-menu")
-          .siblings(".dropdown-toggle")
-          .dropdown("toggle");
-      })
-      .keydown("esc", function () {
-        this.value = "";
-        $(this).change();
-      });
-
-    $("[data-role=magic-overlay]").each(function () {
-      var overlay = $(this),
-        target = $(overlay.data("target"));
-      overlay
-        .css("opacity", 0)
-        .css("position", "absolute")
-        .offset(target.offset())
-        .width(target.outerWidth())
-        .height(target.outerHeight());
-    });
-
-    if ("onwebkitspeechchange" in document.createElement("input")) {
-      var editorOffset = $("#editor").offset();
-
-      $(".voiceBtn")
-        .css("position", "absolute")
-        .offset({
-          top: editorOffset.top,
-          left: editorOffset.left + $("#editor").innerWidth() - 35,
-        });
-    } else {
-      $(".voiceBtn").hide();
-    }
-  }
-
-  function showErrorAlert(reason, detail) {
-    var msg = "";
-    if (reason === "unsupported-file-type") {
-      msg = "Unsupported format " + detail;
-    } else {
-      console.log("error uploading file", reason, detail);
-    }
-    $(
-      '<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>' +
-        "<strong>File upload error</strong> " +
-        msg +
-        " </div>"
-    ).prependTo("#alerts");
-  }
-
-  $(".editor-wrapper").each(function () {
-    var id = $(this).attr("id"); //editor-one
-
-    $(this).wysiwyg({
-      toolbarSelector: '[data-target="#' + id + '"]',
-      fileUploadError: showErrorAlert,
-    });
-  });
-
-  window.prettyPrint;
-  prettyPrint();
-}
-
 /* CROPPER */
 
 function init_cropper() {
@@ -4226,7 +4115,6 @@ function init_echarts() {
 $(document).ready(function () {
   init_sparklines();
   init_sidebar();
-  init_wysiwyg();
   init_InputMask();
   init_cropper();
   init_knob();
