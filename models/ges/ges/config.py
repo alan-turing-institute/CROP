ad63f8b09f3c9647d61e2170ea32efa7b926c9d9
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2022-09-30 11:09:28 +0100

    Update ges/ges/config.py following renaming of env variables in secrets file

--- models/ges/ges/config.py
+++ models/ges/ges/config.py
@@ -1,3 +1,7 @@
+"""
+Python module to read the parameters required to connect to the PostgreSQL database server
+"""
+
 from configparser import ConfigParser
 import os
 
@@ -24,8 +28,16 @@ def config(
 
     # If the same variable is defined also as an environment variable, have that
     # override the value in the file.
-    for key in conf_dict.keys():
-        env_var = "CROP_{}".format(key).upper()
-        if env_var in os.environ:
-            conf_dict[key] = os.environ[env_var]
+    env_var1 = "CROP_SQL_HOST"
+    env_var2 = "CROP_SQL_USERNAME"
+    if env_var1 in os.environ:
+        conf_dict["host"] = os.environ[env_var1]
+        if env_var2 in os.environ:
+            conf_dict["user"] = os.environ[env_var2] + "@" + os.environ[env_var1]
+    env_var = "CROP_SQL_PASS"
+    if env_var in os.environ:
+        conf_dict["password"] = os.environ[env_var]
+    env_var = "CROP_SQL_DBNAME"
+    if env_var in os.environ:
+        conf_dict["dbname"] = os.environ[env_var]
     return conf_dict
