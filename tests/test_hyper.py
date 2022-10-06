98c6edfcdf7356e27debbaf0c47072f741ab5f4c
nbarlowATI <nbarlow@turing.ac.uk>
2022-10-06 16:48:43 +0100

    test irrigation data api parsing

--- tests/test_hyper.py
+++ tests/test_hyper.py
@@ -21,6 +21,9 @@ MOCK_CO2_DATA = json.load(
 MOCK_AIRVELOCITY_DATA = json.load(
     open(os.path.join(THIS_DIR, "data", "Hyper", "hyper_airvelocity_testdata.json"))
 )
+MOCK_IRRIGATION_DATA = json.load(
+    open(os.path.join(THIS_DIR, "data", "Hyper", "hyper_irrigation_testdata.json"))
+)
 
 DT_FROM = datetime.utcnow() + timedelta(days=-1)
 DT_TO = datetime.utcnow()
@@ -74,3 +77,24 @@ def test_get_airvelocity_api_data():
             assert len(v) > 0
             assert "Current" in v.columns
             assert "CurrentDerived" in v.columns
+
+
+def test_get_irrigation_api_data():
+
+    with requests_mock.Mocker() as m:
+        m.get(requests_mock.ANY, json=MOCK_IRRIGATION_DATA)
+        success, error, dfs = get_api_sensor_data(
+            DUMMY_API_KEY, DT_FROM, DT_TO, READINGS_DICTS[3]["columns"]
+        )
+        assert success
+        assert isinstance(dfs, dict)
+        for k, v in dfs.items():
+            assert isinstance(k, str)
+            assert isinstance(v, pd.DataFrame)
+            assert len(v) > 0
+            assert "WaterTemperature" in v.columns
+            assert "WaterPH" in v.columns
+            assert "WaterConductivity" in v.columns
+            assert "WaterOxygen" in v.columns
+            assert "WaterTurbidity" in v.columns
+            assert "WaterPeroxide" in v.columns
