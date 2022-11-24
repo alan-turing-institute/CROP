172cdd00e1b837b714694021643f98da8e0458f5
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2022-11-24 10:42:37 +0000

    improve function documentation

--- models/arima_python/arima/dataAccess.py
+++ models/arima_python/arima/dataAccess.py
@@ -8,7 +8,11 @@ from copy import deepcopy
 
 
 def openConnection():
-    """Connect to the PostgreSQL database server"""
+    """
+    Connect to the PostgreSQL database server.
+    Returns a psycopg2 connection object if
+    connection is successful.
+    """
     conn = None
     try:
         # read DB connection parameters
@@ -43,7 +47,15 @@ def printRowsHead(rows, numrows=0):
 
 
 def getData(query):
-    """Fetch data from the DB based on the type of query"""
+    """
+    Fetch data from the DB based on the type of query.
+
+    Parameters:
+        query: JinjaSql.prepare_query object
+    Returns:
+        rows: a list where each entry corresponds to a
+            different row of the DB table
+    """
     conn = None
     try:
         conn = openConnection()
@@ -52,9 +64,7 @@ def getData(query):
         rows = cur.fetchall()
         printRowsHead(rows, numrows=10)
         cur.close()  # close the communication with the PostgreSQL
-
         logging.info(f"Got data from {query} - returning {len(rows)} rows")
-        print(f"Got data from {query} - returning {len(rows)} rows")
     except (Exception, psycopg2.DatabaseError) as error:
         logging.info(error)
     finally:
@@ -85,9 +95,17 @@ def get_sql_from_template(query, bind_params):
 
 def getTemperatureHumidityData(deltaDays, numRows=None):
     """
-    Fetch temperature and humidity data from the aranet_trh_data
-    table over the specified time period, limited by the specified
+    Fetch temperature and humidity data from the aranet_trh_data table
+    of the DB over the specified time period, limited by the specified
     number of rows.
+
+    Parameters:
+        deltaDays: number of days into the past for which to retrieve the data.
+        numRows: upper limit of number of rows to retrieve. Default is None (no
+            upper limit is set).
+    Returns:
+        data: a list where each entry corresponds to a different row of the DB
+            table, organised by the timestamp column of the aranet_trh_data table.
     """
     today = datetime.datetime.now()
     delta = datetime.timedelta(days=deltaDays)
@@ -130,6 +148,14 @@ def getEnergyData(deltaDays, numRows=None):
     Fetch energy data from the utc_energy_data table
     over the specified time period, limited by the specified
     number of rows.
+
+    Parameters:
+        deltaDays: number of days into the past for which to retrieve the data.
+        numRows: upper limit of number of rows to retrieve. Default is None (no
+            upper limit is set).
+    Returns:
+        data: a list where each entry corresponds to a different row of the DB
+            table, organised by the timestamp column of the utc_energy_data table.
     """
     today = datetime.datetime.now()
     delta = datetime.timedelta(days=deltaDays)
@@ -168,6 +194,24 @@ def getEnergyData(deltaDays, numRows=None):
 
 
 def getTrainingData(numRows=None):
+    """
+    Fetch temperature and humidity data from the aranet_trh_data table
+    and energy data from the utc_energy_data table for training of the
+    ARIMA model.
+    The number of days into the past for which to retrieve data from the
+    DB is specified through "num_days_training" in the config.ini file.
+
+    Parameters:
+        numRows: upper limit of number of rows to retrieve. Default is None (no
+            upper limit is set).
+    Returns:
+        env_data: list containing the temperature and humidity data. Each entry
+            corresponds to a different row in the aranet_trh_data table. Organised
+            by the timestamp column of the aranet_trh_data table.
+        energy_data: list containing the energy data. Each entry corresponds
+            to a different row in the utc_energy_data table. Organised
+            by the timestamp column of the utc_energy_data table.
+    """
     params = config(section="constants")
     num_days_training = params["num_days_training"]
     env_data = getTemperatureHumidityData(deltaDays=num_days_training, numRows=numRows)
