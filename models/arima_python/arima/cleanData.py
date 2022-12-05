4ae6a57f365aaa383539c51e187546a961da9841
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2022-12-05 17:17:55 +0000

    more work on function to clean environment data

--- models/arima_python/arima/cleanData.py
+++ models/arima_python/arima/cleanData.py
@@ -6,10 +6,14 @@ env_data, energy_data = getTrainingData(numRows=50)
 constants = config(section="constants")
 
 
-def cleanEnvData():
+def cleanEnvData(env_data):
     # insert a new column at the end of the dataframe, named "hour_truncated",
     # that takes the truncated hour from the "timestamp" column
-    env_data.insert(len(env_data.columns), "hour_truncated", env_data.timestamp.dt.hour)
+    env_data.insert(
+        len(env_data.columns),
+        "hour_truncated",
+        env_data.timestamp.dt.hour,
+    )
     # insert a new column at the end of the dataframe, named "hour_decimal",
     # that expresses the time of the "timestamp" column in decimal form
     env_data.insert(
@@ -17,5 +21,20 @@ def cleanEnvData():
         "hour_decimal",
         env_data.timestamp.dt.hour
         + env_data.timestamp.dt.minute / constants["secs_per_min"],
+    )
+    # create a new column, named "timedelta_in_secs", that expresses
+    # the time difference, in seconds, between a timestamp and
+    # itself rounded to the hour
+    env_data["timedelta_in_secs"] = env_data["timestamp"].apply(
+        lambda x: abs((x - x.round(freq="H")).total_seconds())
+    )
+    # create a new column, named "timestamp_plus_minus", where any
+    # timestamp 15min before or after the hour is given the timestamp
+    # of the rounded hour. Times outside this range are assigned None.
+    env_data["timestamp_plus_minus"] = env_data.apply(
+        lambda x: x["timestamp"].round(freq="H")
+        if x["timedelta_in_secs"] <= 15 * constants["secs_per_min"]
+        else None,
+        axis=1,
     )
     return env_data
