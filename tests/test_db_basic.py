d65e618eb7a5118394d871ec25473244f568c433
nbarlowATI <nbarlow@turing.ac.uk>
2022-12-08 14:44:41 +0000

    remove empty test file and rename basic db tests

--- tests/test_db_basic.py
+++ tests/test_db_basic.py
@@ -3,29 +3,18 @@ Test db.py module
 """
 
 
-from core.constants import SQL_CONNECTION_STRING
+from core.constants import SQL_CONNECTION_STRING, SQL_TEST_DBNAME
+
 from core.db import (
-    create_database,
     connect_db,
-    drop_db,
     check_database_structure,
 )
-
-
-TEST_DB_NAME = "fake_db"
-
-
-def test_create_database():
-
-    # Test create new db
-    created, log = create_database(SQL_CONNECTION_STRING, TEST_DB_NAME)
-    assert created, log
 
 
 def test_connect_db():
 
     # Try to connect to an engine that exists
-    status, log, engine = connect_db(SQL_CONNECTION_STRING, TEST_DB_NAME)
+    status, log, engine = connect_db(SQL_CONNECTION_STRING, SQL_TEST_DBNAME)
     assert status, log
 
     # Try to connect to a db that does not exists:
@@ -37,16 +26,9 @@ def test_connect_db():
 
 def test_check_database_structure():
 
-    status, log, engine = connect_db(SQL_CONNECTION_STRING, TEST_DB_NAME)
+    status, log, engine = connect_db(SQL_CONNECTION_STRING, SQL_TEST_DBNAME)
     assert status, log
     assert engine is not None
 
     good, log = check_database_structure(engine)
     assert good, log
-
-
-def test_drop_db():
-
-    # Test drop db
-    success, log = drop_db(SQL_CONNECTION_STRING, TEST_DB_NAME)
-    assert success, log
