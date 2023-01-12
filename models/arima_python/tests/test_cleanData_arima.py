3eae9ab318b54483c4c2c932ae220c26afc5b6f7
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-01-12 21:36:53 +0000

    test format of clean data

--- models/arima_python/tests/test_cleanData_arima.py
+++ models/arima_python/tests/test_cleanData_arima.py
@@ -9,3 +9,24 @@ energy_raw = pd.read_pickle("tests/data/utc_energy_raw.pkl")
 # import the processed data
 env_clean = pd.read_pickle("tests/data/aranet_trh_processed.pkl")
 energy_clean = pd.read_pickle("tests/data/utc_energy_processed.pkl")
+
+# set the list of temperature/rel humidity sensors
+keys_clean = list(env_clean.keys())
+cleanData.sensors_list = keys_clean
+
+env_data, energy_data = cleanData.cleanData(env_raw, energy_raw)
+
+
+def test_format_cleanData():
+    """
+    Test that the format of the post-processed data is
+    the expected one.
+    """
+    # chect that variable types are correct
+    assert isinstance(env_data, dict)
+    keys = list(env_data.keys())
+    for ii in range(0, len(keys)):
+        assert isinstance(env_data[keys[ii]], pd.DataFrame)
+    assert isinstance(energy_data, pd.DataFrame)
+    # check that the keys are the expected ones
+    assert all(item in keys for item in keys_clean)
