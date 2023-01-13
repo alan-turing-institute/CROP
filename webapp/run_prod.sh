db7f44e6f14d76a4d2215e442f0b183abbf7695a
nbarlowATI <nbarlow@turing.ac.uk>
2023-01-13 11:27:35 +0000

    have run scripts for both dev server and gunicorn+nginx

--- webapp/run_prod.sh
+++ webapp/run_prod.sh
@@ -11,14 +11,11 @@ export PYTHONPATH=`pwd`"/..:${PYTHONPATH}"
 if test -f "../.secrets/crop.sh"; then
     source ../.secrets/crop.sh
 fi
-npm install
 
-if [ -n "$1" ] && [ "$1" -gt "-1" ]
-then
-    bport=$1
-else
-    bport=5000
-fi
 
 npm install
-flask run --host=0.0.0.0 --port $bport
+
+# start nginx HTTP server / reverse proxy
+nginx
+# start gunicorn WSGI server
+gunicorn --limit-request-line 0 'crop_app:app'
