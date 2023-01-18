4fb6991c1a80d236d22f23781d4b4e05853a108b
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-01-18 17:01:16 +0000

    clean up testing of clean data

--- models/arima_python/tests/test_cleanData_arima.py
+++ models/arima_python/tests/test_cleanData_arima.py
@@ -8,11 +8,7 @@ energy_raw = pd.read_pickle("tests/data/utc_energy_raw.pkl")
 # import the processed data - this is the baseline we compare against
 env_clean = pd.read_pickle("tests/data/aranet_trh_processed.pkl")
 energy_clean = pd.read_pickle("tests/data/utc_energy_processed.pkl")
-
-# set the list of temperature/rel humidity (TRH) sensors
-# this is done here because "sensors_list" can be specified by the user via the config file
 keys_clean = list(env_clean.keys())
-cleanData.sensors_list = keys_clean
 
 # column names TRH data should contain
 colnames_env = list(env_clean[keys_clean[0]].columns)
@@ -20,45 +16,51 @@ colnames_env = list(env_clean[keys_clean[0]].columns)
 # column names energy data should contain
 colnames_energy = list(energy_clean.columns)
 
-# call the function to process the raw data
+# set the list of temperature/rel humidity (TRH) sensors
+# this is done here because "sensors_list" can be specified by the user via the config file
+cleanData.sensors_list = keys_clean
+
+# now call the function to process the raw data
 env_data, energy_data = cleanData.cleanData(env_raw, energy_raw)
 keys = list(env_data.keys())
 
 
 def test_format_cleanData():
     """
-    Test that the format of the pre-processed data is
+    Test that the format of the processed data is
     the expected one.
     """
     # chect that variable types are correct
     assert isinstance(env_data, dict)
-    for ii in range(0, len(keys)):
-        assert isinstance(env_data[keys[ii]], pd.DataFrame)
+    for sensor in keys:
+        assert isinstance(env_data[sensor], pd.DataFrame)
     assert isinstance(energy_data, pd.DataFrame)
-    # check that the keys are the expected ones
-    assert all(item in keys for item in keys_clean)
+    # check that the keys are the expected ones, regardless of order
+    assert sorted(keys) == sorted(keys_clean)
 
 
 def test_columns_cleanData():
     """
-    Test that the pre-processed dataframes contain the
+    Test that the processed dataframes contain the
     expected columns.
     """
-    # loop through every sensor (key) in the TRH data
-    for ii in range(0, len(keys)):
-        assert all(item in env_data[keys[ii]].columns for item in colnames_env)
+    # note that in the way this is coded here the dataframes must have
+    # at least the expected columns (they can also have extra columns).
+    # Loop through every sensor (key) in the TRH data
+    for sensor in keys:
+        assert all(item in env_data[sensor].columns for item in colnames_env)
     # now test the energy data
     assert all(item in energy_data.columns for item in colnames_energy)
 
 
 def test_timestamps_cleanData():
     """
-    Test that the pre-processed TRH and Energy dataframes
+    Test that the processed TRH and Energy dataframes
     have the same timestamps, that the timestamp vector is
     monotonically increasing and that timedelta between
     timestamps is unique.
     """
-    # check that the timestamp vector is the same for all data
+    # check that the timestamp vector is the same for all dataframes
     for ii in range(1, len(keys)):
         timestamp1 = env_data[keys[ii - 1]].timestamp
         timestamp2 = env_data[keys[ii]].timestamp
@@ -75,11 +77,11 @@ def test_timestamps_cleanData():
 
 def test_data_cleanData():
     """
-    Test that the data in the pre-processed dataframes
+    Test that the data in the processed dataframes
     is strictly equal to the expected one.
     """
     # compare sensor by sensor and column by column
-    for sensor in keys_clean:
+    for sensor in keys:
         for column in colnames_env:
             assert env_data[sensor][column].equals(env_clean[sensor][column])
     for column in colnames_energy:
