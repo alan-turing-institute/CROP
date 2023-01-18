95b076db48558797380b47374d2d6c0cb86bbea2
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-01-18 11:04:23 +0000

    test column names of clean data

--- models/arima_python/tests/test_cleanData_arima.py
+++ models/arima_python/tests/test_cleanData_arima.py
@@ -6,15 +6,17 @@ import arima.config as config
 env_raw = pd.read_pickle("tests/data/aranet_trh_raw.pkl")
 energy_raw = pd.read_pickle("tests/data/utc_energy_raw.pkl")
 
-# import the processed data
+# import the processed data - this is the baseline we compare against
 env_clean = pd.read_pickle("tests/data/aranet_trh_processed.pkl")
 energy_clean = pd.read_pickle("tests/data/utc_energy_processed.pkl")
 
-# set the list of temperature/rel humidity sensors
+# set the list of temperature/rel humidity (TRH) sensors
 keys_clean = list(env_clean.keys())
 cleanData.sensors_list = keys_clean
 
+# call the function to process the raw data
 env_data, energy_data = cleanData.cleanData(env_raw, energy_raw)
+keys = list(env_data.keys())
 
 
 def test_format_cleanData():
@@ -24,9 +26,33 @@ def test_format_cleanData():
     """
     # chect that variable types are correct
     assert isinstance(env_data, dict)
-    keys = list(env_data.keys())
     for ii in range(0, len(keys)):
         assert isinstance(env_data[keys[ii]], pd.DataFrame)
     assert isinstance(energy_data, pd.DataFrame)
     # check that the keys are the expected ones
     assert all(item in keys for item in keys_clean)
+
+
+def test_columns_cleanData():
+    """
+    Test that the post-processed dataframes contain the
+    expected columns.
+    """
+    # expected column names for TRH data
+    colnames = [
+        "timestamp",
+        "temperature",
+        "humidity",
+    ]
+    # loop through every sensor (key) in the TRH data
+    for ii in range(0, len(keys)):
+        assert all(item in env_data[keys[ii]].columns for item in colnames)
+    # expected column names for energy data
+    colnames = [
+        "timestamp",
+        "EnergyCC",
+        "EnergyCP",
+        "EnergyCC_MA",
+        "EnergyCP_MA",
+    ]
+    assert all(item in energy_data for item in colnames)
