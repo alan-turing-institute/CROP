8d3973346dd74c519879748020870559812aa3a5
Markus Hauru <mhauru@turing.ac.uk>
2023-01-18 17:55:41 +0000

    Improvements to Pulumi setup, still WIP

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import pulumi_azure_native.dbforpostgresql as dbforpostgresql
 import pulumi_azure_native.insights as insights
 import pulumi_azure_native.resources as resource
 import pulumi_azure_native.sql as sql
@@ -6,16 +7,19 @@ import pulumi_azure_native.web as web
 from pulumi import Config, Output, asset, export
 from pulumi_azure_native.storage import BlobContainer, PublicAccess, StorageAccount
 
+config = Config()
+
 username = "pulumi"
 webapp_docker_url = "turingcropapp/webapp:dev"
+resource_name_prefix = "crop-dev"
+sql_server_user = "cropdbadmin"
+sql_server_password = config.require("sql-server-password")
+sql_db_name = "app_db"
 
-config = Config()
-pwd = config.require("sql-server-password")
-
-resource_group = resource.ResourceGroup("crop-dev-rg")
+resource_group = resource.ResourceGroup(f"{resource_name_prefix}-rg")
 
 app_service_plan = web.AppServicePlan(
-    "crop-dev-web-asp",
+    f"{resource_name_prefix}-web-asp",
     resource_group_name=resource_group.name,
     kind="Linux",
     reserved=True,
@@ -26,44 +30,50 @@ app_service_plan = web.AppServicePlan(
 )
 
 app_insights = insights.Component(
-    "crop-dev-web-ai",
+    f"{resource_name_prefix}-web-ai",
     application_type=insights.ApplicationType.WEB,
     kind="web",
     resource_group_name=resource_group.name,
 )
 
-sql_server = sql.Server(
-    "crop-dev-sql-server",
+sql_server_name = f"{resource_name_prefix}-postgresql"
+sql_server = dbforpostgresql.Server(
+    sql_server_name,
+    server_name=sql_server_name,
     resource_group_name=resource_group.name,
-    administrator_login=username,
-    administrator_login_password=pwd,
-    version="12.0",
-)
-
-database = sql.Database(
-    "crop-dev-sql-db",
-    resource_group_name=resource_group.name,
-    server_name=sql_server.name,
-    sku=sql.SkuArgs(
-        name="S0",
+    properties=dbforpostgresql.ServerPropertiesForDefaultCreateArgs(
+        administrator_login=sql_server_user,
+        administrator_login_password=sql_server_password,
+        create_mode="Default",
+        minimal_tls_version="TLS1_2",
+        ssl_enforcement=dbforpostgresql.SslEnforcementEnum.ENABLED,
+        storage_profile=dbforpostgresql.StorageProfileArgs(
+            backup_retention_days=14,
+            geo_redundant_backup="Disabled",
+            storage_autogrow=dbforpostgresql.StorageAutogrow.ENABLED,
+            storage_mb=5120,
+        ),
+    ),
+    sku=dbforpostgresql.SkuArgs(
+        capacity=2,
+        family="Gen5",
+        name="B_Gen5_2",
+        tier="Basic",
     ),
 )
 
-connection_string = Output.concat(
-    "Server=tcp:",
-    sql_server.name,
-    ".database.windows.net;initial ",
-    "catalog=",
-    database.name,
-    ";user ID=",
-    username,
-    ";password=",
-    pwd,
-    ";Min Pool Size=0;Max Pool Size=30;Persist Security Info=true;",
+sql_db = dbforpostgresql.Database(
+    sql_db_name,
+    charset="UTF8",
+    collation="English_United States.1252",
+    database_name=sql_db_name,
+    resource_group_name=resource_group.name,
+    server_name=sql_server.name,
 )
 
+
 app = web.WebApp(
-    "crop-dev-web-as",
+    f"{resource_name_prefix}-webapp",
     resource_group_name=resource_group.name,
     server_farm_id=app_service_plan.id,
     site_config=web.SiteConfigArgs(
@@ -85,13 +95,21 @@ app = web.WebApp(
                 name="WEBSITES_ENABLE_APP_SERVICE_STORAGE",
                 value="false",
             ),
-        ],
-        connection_strings=[
-            web.ConnStringInfoArgs(
-                name="db",
-                type="SQLAzure",
-                connection_string=connection_string,
-            )
+            web.NameValuePairArgs(
+                name="DOCKER_REGISTRY_SERVER_URL", value="https://index.docker.io/v1"
+            ),
+            web.NameValuePairArgs(
+                name="CROP_SQL_HOST",
+                value=f"{sql_server_name}.postgres.database.azure.com",
+            ),
+            web.NameValuePairArgs(name="CROP_SQL_PASS", value=sql_server_password),
+            web.NameValuePairArgs(name="CROP_SQL_PORT", value="5432"),
+            web.NameValuePairArgs(
+                name="CROP_SQL_USER",
+                value=f"{sql_server_user}@{sql_server_name}",
+            ),
+            web.NameValuePairArgs(name="CROP_SQL_USERNAME", value=sql_server_user),
+            web.NameValuePairArgs(name="CROP_SQL_DBNAME", value=sql_db_name),
         ],
         always_on=True,
         linux_fx_version=f"DOCKER|{webapp_docker_url}",
