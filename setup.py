10e9d53b2c7809673fbab192481cb8c3468fd5f0
nbarlowATI <nbarlow@turing.ac.uk>
2023-02-06 08:42:15 +0000

    restore original setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+import os
 from setuptools import setup
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 setup(
     name="cropcore",
-    version="3.0.0",
+    version="3.0",
+    description="core functionality for the CROP digital twin",
     url="https://github.com/alan-turing-institute/CROP",
-    description="digital twin of an underground farm",
+    author="The Alan Turing Institute Research Engineering Group",
+    license="MIT",
+    packages=["cropcore"],
+    package_dir={"cropcore": "core"},
     install_requires=required,
-    packages=['cropcore'],
-    package_dir={"cropcore": "core"}
+    zip_safe=False,
 )
