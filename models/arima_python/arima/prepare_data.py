c914ba544836c34df384e9b45641adfe3eed6dd5
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-13 16:49:21 +0000

    add description to function to standardise timestamps

--- models/arima_python/arima/prepare_data.py
+++ models/arima_python/arima/prepare_data.py
@@ -7,6 +7,29 @@ arima_config = config(section="arima")
 
 
 def standardize_timestamp(timestamp):
+    """
+    Standardize the input timestamp according to the
+    time at which the farm daily cycle starts.
+    Specify the start time of the farm cycle using the
+    `farm_cycle_start` parameter in config.ini.
+
+    Parameters:
+        timestamp: a datetime object or equivalent
+            (e.g. pandas Timestamp).
+
+    Returns:
+        timestamp: the input timestamp standardised according
+            the time at which the farm cycle starts.
+            - If the time of the input timestamp is >=
+            `farm_cycle_start`, the output timestamp is the date
+            of the input timestamp at time `farm_cycle_start`.
+            - If the time of the input timestamp is <=
+            (`farm_cycle_start` - 12 hours), the output timestamp
+            is the date of the input timestamp minus one day at
+            time `farm_cycle_start`.
+            - Otherwise, the output timestamp is the date of the
+            input timestamp at time (`farm_cycle_start` - 12 hours).
+    """
     farm_cycle_start = arima_config[
         "farm_cycle_start"
     ]  # time at which the farm cycle starts
