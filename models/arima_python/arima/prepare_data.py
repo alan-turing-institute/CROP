c8296c2cc47284c3b562b0eab5e2cbbdd3e14a38
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-13 16:48:40 +0000

    rename the function to standardise timestamps

--- models/arima_python/arima/prepare_data.py
+++ models/arima_python/arima/prepare_data.py
@@ -6,26 +6,22 @@ data_config = config(section="data")
 arima_config = config(section="arima")
 
 
-def standardize_latest_timestamp(latest_timestamp):
+def standardize_timestamp(timestamp):
     farm_cycle_start = arima_config[
         "farm_cycle_start"
-    ]  # hour at which the farm cycle starts
+    ]  # time at which the farm cycle starts
     farm_cycle_start = datetime.strptime(farm_cycle_start, "%Hh%Mm%Ss")
-    farm_cycle_start = datetime.combine(
-        latest_timestamp.date(), farm_cycle_start.time()
-    )
+    farm_cycle_start = datetime.combine(timestamp.date(), farm_cycle_start.time())
 
-    if latest_timestamp >= farm_cycle_start:
-        latest_timestamp = farm_cycle_start
-    elif latest_timestamp <= (
+    if timestamp >= farm_cycle_start:
+        timestamp = farm_cycle_start
+    elif timestamp <= (
         farm_cycle_start - timedelta(hours=constants["hrs_per_day"] / 2)
     ):
-        latest_timestamp = datetime.combine(
-            (latest_timestamp - timedelta(days=1)).date(),
+        timestamp = datetime.combine(
+            (timestamp - timedelta(days=1)).date(),
             farm_cycle_start.time(),
         )
     else:
-        latest_timestamp = farm_cycle_start - timedelta(
-            hours=constants["hrs_per_day"] / 2
-        )
-    return latest_timestamp
+        timestamp = farm_cycle_start - timedelta(hours=constants["hrs_per_day"] / 2)
+    return timestamp
