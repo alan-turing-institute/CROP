fbf67bd8a7e09be7923a8626bd7ade045da27720
mastoffel <martin.adam.stoffel@gmail.com>
2023-03-14 09:44:19 +0000

    Get energy data with sqlalchemy

--- models/arima_python/arima/data_access_sqlalchemy.py
+++ models/arima_python/arima/data_access_sqlalchemy.py
@@ -1,10 +1,57 @@
 import logging
 import datetime
-#from .config import config
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
-from copy import deepcopy
 import pandas as pd
 
-from arima_utils import get_sqlalchemy_session
+from .config import config
+from sqlalchemy import desc, asc, exc, func
 
+from cropcore.db import connect_db, session_open, session_close
+from cropcore.structure import (
+    SensorClass,
+    ReadingsAranetTRHClass,
+    ReadingsEnergyClass
+)
+    
+from .arima_utils import get_sqlalchemy_session
+
+logger = logging.getLogger(__name__)
+
+def get_energy_data(delta_days=100, num_rows=20, session=None):
+    """ Fetch energy data from the utc_energy_data table
+    over the specified time period, limited by the specified
+    number of rows.
+
+    Args:
+        delta_days (int, optional): Number of days in the past from which to retrieve data. Defaults to 100.
+        num_rows (int, optional): Number of rows to limit the data to. Defaults to 20.
+        session (_type_, optional): _description_. Defaults to None.
+
+    Returns:
+        data: A pandas dataframe with each row corresponding to a different row of the DB table, 
+        sorted by the timestamp column of the utc_energy_data table.
+    """
+    if not session:
+        session = get_sqlalchemy_session()
+    date_to = datetime.datetime.now()
+    delta = datetime.timedelta(days=delta_days)
+    data_from = date_to - delta
+    query = (
+        # retrieve all columns from the ReadingsEnergyClass table
+        session.query(
+            func.to_char(ReadingsEnergyClass.timestamp, 'YYYY-MM-DD HH24:MI:SS').label('timestamp'),
+            ReadingsEnergyClass.electricity_consumption,
+            ReadingsEnergyClass.time_created,
+            ReadingsEnergyClass.time_updated,
+            ReadingsEnergyClass.sensor_id,
+            ReadingsEnergyClass.id
+            )
+        .filter(ReadingsEnergyClass.timestamp > data_from)
+        .order_by(asc(ReadingsEnergyClass.timestamp))
+        .limit(num_rows)
+    )
+    result = query.all()
+    session_close(session)
+    return result
+
+df = pd.DataFrame(get_energy_data())
+print(df.head(5))
