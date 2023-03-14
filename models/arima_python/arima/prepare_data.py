ab73e2171780e1370b95c535ff2c2a6e8184220a
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-14 11:27:51 +0000

    start working on the `prepare_data` function

--- models/arima_python/arima/prepare_data.py
+++ models/arima_python/arima/prepare_data.py
@@ -6,7 +6,7 @@ data_config = config(section="data")
 arima_config = config(section="arima")
 
 
-def standardize_timestamp(timestamp):
+def standardize_timestamp(timestamp: datetime) -> datetime:
     """
     Standardize the input timestamp according to the
     time at which the farm daily cycle starts.
@@ -33,6 +33,7 @@ def standardize_timestamp(timestamp):
     farm_cycle_start = arima_config[
         "farm_cycle_start"
     ]  # time at which the farm cycle starts
+    # parse string into a datetime object
     farm_cycle_start = datetime.strptime(farm_cycle_start, "%Hh%Mm%Ss")
     farm_cycle_start = datetime.combine(timestamp.date(), farm_cycle_start.time())
 
@@ -48,3 +49,25 @@ def standardize_timestamp(timestamp):
     else:
         timestamp = farm_cycle_start - timedelta(hours=constants["hrs_per_day"] / 2)
     return timestamp
+
+
+def prepare_data(env_clean, energy_clean):
+    # obtain the standardized timestamp.
+    # note that both `env_clean` and `energy_clean` are indexed by the same timestamps.
+    timestamp_standardized = standardize_timestamp(energy_clean.index[-1])
+    # keep only the observations whose timestamp is smaller or equal to the
+    # standardized timestamp
+    keys = list(env_clean.keys())
+    for key in keys:
+        env_clean[key].drop(
+            env_clean[key][env_clean[key].index > timestamp_standardized].index,
+            inplace=True,
+        )
+    energy_clean.drop(
+        energy_clean[energy_clean.index > timestamp_standardized].index,
+        inplace=True,
+    )
+    # compute the total hourly energy consumption, given the sampling frequency
+    # of the `utc_energy_data` table
+    freq_energy_data = data_config["freq_energy_data"]
+    freq_energy_data = datetime.strptime(freq_energy_data, "%Hh%Mm%Ss")
