9d45233a8fa4cfc0a551e53f3fa531544f65d227
mastoffel <martin.adam.stoffel@gmail.com>
2023-03-17 11:09:35 +0000

    Fix test for flexible arima data access

--- models/arima_python/tests/test_data_access_arima.py
+++ models/arima_python/tests/test_data_access_arima.py
@@ -2,8 +2,6 @@ from ..arima.arima_utils import get_sqlalchemy_session
 #from models.ges.ges.ges_utils import get_sqlalchemy_session
 from cropcore.db import connect_db, session_open, session_close
 from cropcore.model_data_access import (
-    get_energy_data,
-    get_temperature_humidity_data,
     get_training_data
 )
 
@@ -15,13 +13,21 @@ def test_connection():
     assert conn is not None
     session_close(conn)
     
-def test_get_energy_data():
+def test_get_training_data():
     """
-    Test that the format of the energy data fetched
+    Test that the format of the training data fetched
     from the DB is correct.
+    The training data is fetched from the "aranet_trh_data"
+    and "utc_energy_data" tables.
     """
-    
-    energy_data = get_energy_data(delta_days=100, num_rows=20)
+    # fetch 50 rows of training data
+    num_rows = 50
+    env_data, energy_data = get_training_data(num_rows=num_rows, config_sections=["env_data", "energy_data"])
+    # check that the dataframes have the correct size
+    num_cols = 8
+    assert env_data.shape == (num_rows, num_cols)
+    num_cols = 6
+    assert energy_data.shape == (num_rows, num_cols)
     
     # check that column names are the expected ones
     colnames = [
@@ -47,15 +53,7 @@ def test_get_energy_data():
         [energy_data[item].dtypes == datatypes[item] for item in datatypes.keys()]
     )
     
-def test_get_temperature_humidity_data():
-    """
-    Test that the format of the temperature and humidity data fetched
-    from the DB is correct.
-    """
-    
-    env_data = get_temperature_humidity_data(delta_days=100, num_rows=20)
-    
-    # check that the dataframes have the expected column names
+        # check that the dataframes have the expected column names
     colnames = [
         "name",
         "id",
@@ -80,19 +78,3 @@ def test_get_temperature_humidity_data():
         "time_updated": "O",
     }
     assert all([env_data[item].dtypes == datatypes[item] for item in datatypes.keys()])
-    
-def test_get_training_data():
-    """
-    Test that the format of the training data fetched
-    from the DB is correct.
-    The training data is fetched from the "aranet_trh_data"
-    and "utc_energy_data" tables.
-    """
-    # fetch 50 rows of training data
-    num_rows = 50
-    env_data, energy_data = get_training_data(num_rows=num_rows)
-    # check that the dataframes have the correct size
-    num_cols = 8
-    assert env_data.shape == (num_rows, num_cols)
-    num_cols = 6
-    assert energy_data.shape == (num_rows, num_cols)
