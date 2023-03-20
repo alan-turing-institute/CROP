e107d085a9d0e5be99fe47fef28d653f9d6eaed8
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-20 14:54:10 +0000

    Revert "Sqlalchemy to arima"

--- models/arima_python/tests/test_data_access_arima.py
+++ models/arima_python/tests/test_data_access_arima.py
@@ -1,12 +1,4 @@
-import arima.arima_utils as arima_utils
-
-# from models.ges.ges.ges_utils import get_sqlalchemy_session
-from arima.data_access import (
-    data_config,
-    get_energy_data,
-    get_temperature_humidity_data,
-    get_training_data,
-)
+import arima.data_access as data_access
 import pytest
 
 
@@ -14,20 +6,40 @@ def test_connection():
     """
     Test PostgreSQL connection
     """
-    conn = arima_utils.get_sqlalchemy_session()
+    conn = data_access.open_connection()
     assert conn is not None
-    arima_utils.session_close(conn)
+    data_access.close_connection(conn)
 
 
-def test_get_energy_data():
+def test_get_training_data():
     """
-    Test that the format of the energy data fetched
+    Test that the format of the training data fetched
     from the DB is correct.
+    The training data is fetched from the "aranet_trh_data"
+    and "utc_energy_data" tables.
     """
-
-    energy_data = get_energy_data(delta_days=100, num_rows=20)
-
-    # check that column names are the expected ones
+    conn = data_access.open_connection()  # open PostgreSQL connection
+    # fetch 50 rows of training data
+    num_rows = 50
+    env_data, energy_data = data_access.get_training_data(num_rows=num_rows)
+    data_access.close_connection(conn)  # close PostreSQL connection
+    # check that the dataframes have the correct size
+    num_cols = 8
+    assert env_data.shape == (num_rows, num_cols)
+    num_cols = 6
+    assert energy_data.shape == (num_rows, num_cols)
+    # check that the dataframes have the expected column names
+    colnames = [
+        "name",
+        "id",
+        "sensor_id",
+        "timestamp",
+        "temperature",
+        "humidity",
+        "time_created",
+        "time_updated",
+    ]
+    assert all(item in colnames for item in env_data.columns)
     colnames = [
         "timestamp",
         "electricity_consumption",
@@ -37,8 +49,18 @@ def test_get_energy_data():
         "id",
     ]
     assert all(item in colnames for item in energy_data.columns)
-
     # check that the dataframe column datatypes are the expectd ones
+    datatypes = {
+        "name": "O",
+        "id": "int64",
+        "sensor_id": "int64",
+        "timestamp": "<M8[ns]",  # note no time-zone information
+        "temperature": "float64",
+        "humidity": "float64",
+        "time_created": "<M8[ns]",  # note no time-zone information
+        "time_updated": "O",
+    }
+    assert all([env_data[item].dtypes == datatypes[item] for item in datatypes.keys()])
     datatypes = {
         "timestamp": "<M8[ns]",  # note no time-zone information
         "electricity_consumption": "float64",
@@ -52,63 +74,11 @@ def test_get_energy_data():
     )
 
 
-def test_get_temperature_humidity_data():
-    """
-    Test that the format of the temperature and humidity data fetched
-    from the DB is correct.
-    """
-
-    env_data = get_temperature_humidity_data(delta_days=100, num_rows=20)
-
-    # check that the dataframes have the expected column names
-    colnames = [
-        "name",
-        "id",
-        "sensor_id",
-        "timestamp",
-        "temperature",
-        "humidity",
-        "time_created",
-        "time_updated",
-    ]
-    assert all(item in colnames for item in env_data.columns)
-
-    # check that the dataframe column datatypes are the expectd ones
-    datatypes = {
-        "name": "O",
-        "id": "int64",
-        "sensor_id": "int64",
-        "timestamp": "<M8[ns]",  # note no time-zone information
-        "temperature": "float64",
-        "humidity": "float64",
-        "time_created": "<M8[ns]",  # note no time-zone information
-        "time_updated": "O",
-    }
-    assert all([env_data[item].dtypes == datatypes[item] for item in datatypes.keys()])
-
-
-def test_get_training_data():
-    """
-    Test that the format of the training data fetched
-    from the DB is correct.
-    The training data is fetched from the "aranet_trh_data"
-    and "utc_energy_data" tables.
-    """
-    # fetch 50 rows of training data
-    num_rows = 50
-    env_data, energy_data = get_training_data(num_rows=num_rows)
-    # check that the dataframes have the correct size
-    num_cols = 8
-    assert env_data.shape == (num_rows, num_cols)
-    num_cols = 6
-    assert energy_data.shape == (num_rows, num_cols)
-
-
 def test_num_days_training():
     """
     Test that a ValueError is raised if the `num_days_training`
     parameter in config.ini is set to a value greater than 365.
     """
     with pytest.raises(ValueError):
-        data_config["num_days_training"] = 366
-        get_training_data(num_rows=50)
+        data_access.data_config["num_days_training"] = 366
+        data_access.get_training_data(num_rows=50)
