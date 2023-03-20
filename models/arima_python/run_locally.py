999bd1983856dc8a1639fc024afffeeb7d4adbad
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-20 13:05:39 +0000

    update `run_locally.py`

--- models/arima_python/run_locally.py
+++ models/arima_python/run_locally.py
@@ -1,6 +1,9 @@
 def main() -> None:
     from arima.data_access import get_training_data
     from arima.clean_data import clean_data
+    from arima.prepare_data import prepare_data
+    from arima.arima_pipeline import arima_pipeline
+    from collections import defaultdict
     import pickle
     import logging, coloredlogs
     import sys
@@ -14,19 +17,48 @@ def main() -> None:
     coloredlogs.ColoredFormatter(field_styles=field_styles)
     coloredlogs.install(level="INFO")
 
-    env_raw, energy_raw = get_training_data(num_rows=500)
+    # fetch training data from the database
+    env_data, energy_data = get_training_data(num_rows=30000)
 
+    # save the raw training data to disk
     with open("dump/env_raw.pkl", "wb") as handle:
-        pickle.dump(env_raw, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(env_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
     with open("dump/energy_raw.pkl", "wb") as handle:
-        pickle.dump(energy_raw, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(energy_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    env_clean, energy_clean = clean_data(env_raw, energy_raw)
+    # clean the training data
+    env_data, energy_data = clean_data(env_data, energy_data)
 
+    # save the clean data to disk
     with open("dump/env_clean.pkl", "wb") as handle:
-        pickle.dump(env_clean, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(env_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
     with open("dump/energy_clean.pkl", "wb") as handle:
-        pickle.dump(energy_clean, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(energy_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # prepare the clean data for the ARIMA model
+    env_data, energy_data = prepare_data(env_data, energy_data)
+
+    # save the prepared data to disk
+    with open("dump/env_prepared.pkl", "wb") as handle:
+        pickle.dump(env_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open("dump/energy_prepared.pkl", "wb") as handle:
+        pickle.dump(energy_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # run the ARIMA pipeline for every temperature sensor
+    sensor_names = list(env_data.keys())
+    forecast_results = defaultdict(dict)
+
+    # loop through every sensor
+    for sensor in sensor_names:
+        temperature = env_data[sensor]["temperature"]
+        mean_forecast, conf_int, metrics = arima_pipeline(temperature)
+        forecast_results[sensor]["mean_forecast"] = mean_forecast
+        forecast_results[sensor]["conf_int"] = conf_int
+        forecast_results[sensor]["metrics"] = metrics
+
+    # save the forecast results to disk
+    with open("dump/temperature_forecast.pkl", "wb") as handle:
+        pickle.dump(forecast_results, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
