0af923c000387c6c7c349597c3fee04593aec99f
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-24 16:59:38 +0000

    experimenting with the arima run_pipeline

--- models/arima_python/run_pipeline.py
+++ models/arima_python/run_pipeline.py
@@ -1,4 +1,9 @@
-from cropcore.model_data_access import get_training_data
+from cropcore.model_data_access import (
+    get_training_data,
+    insert_model_run,
+    insert_model_product,
+    insert_model_predictions,
+)
 from arima.clean_data import clean_data
 from arima.prepare_data import prepare_data
 from arima.arima_pipeline import arima_pipeline
@@ -11,6 +16,7 @@ from arima.arima_utils import (
 import logging, coloredlogs
 import pandas as pd
 import sys
+from datetime import datetime
 
 
 def run_pipeline() -> None:
@@ -21,7 +27,7 @@ def run_pipeline() -> None:
     coloredlogs.install(level="INFO")
 
     # fetch training data from the database
-    env_data, energy_data = get_training_data()
+    env_data, energy_data = get_training_data(num_rows=1000)
 
     # clean the training data
     env_data, energy_data = clean_data(env_data, energy_data)
@@ -42,18 +48,40 @@ def run_pipeline() -> None:
             prediction_parameters.append((product_id, result_at_hour, prediction_index))
         return prediction_parameters
 
+    measure_names = [
+        "Mean Temperature (Degree Celcius)",
+        "Lower Bound Temperature (Degree Celcius)",
+        "Upper Bound Temperature (Degree Celcius)",
+    ]
     # loop through every sensor
     for sensor in sensor_names:
-        # sensor_id = get_sensor_id(name=sensor)
+        sensor_id = get_sensor_id(sensor_name=sensor)
         temperature = env_data[sensor]["temperature"]
         mean_forecast, conf_int, metrics = arima_pipeline(temperature)
-        # run_id = insert_model_run(sensor_id, model_id, dt.now())
-        # mean_measure_id = get_measure_id("Mean Temperature (Degree Celcius)")
-        # lower_measure_id = get_measure_id("Lower Bound Temperature (Degree Celcius)")
-        # upper_measure_id = get_measure_id("Upper Bound Temperature (Degree Celcius)")
-        # mean_product_id = insert_model_product(run_id, measure_id)
-        # mean_forecast = process_output(mean_forecast, mean_product_id)
-        # insert_model_predictions(mean_forecast)
+        session.begin()
+        try:
+            run_id = insert_model_run(sensor_id, model_id, datetime.now())
+            for measure_name in measure_names:
+                measure_id = get_measure_id(measure_name)
+                product_id = insert_model_product(run_id, measure_id)
+                print(f"Run ID: {run_id}")
+                print(f"Model ID: {model_id}")
+                print(f"Product ID: {product_id}")
+                print(f"Sensor ID: {sensor_id}")
+                print(f"Measure ID: {measure_id}")
+                if "Mean" in measure_name:
+                    result = process_output(mean_forecast, product_id)
+                elif "Lower" in measure_name:
+                    result = process_output(conf_int["mean_ci_lower"], product_id)
+                elif "Upper" in measure_name:
+                    result = process_output(conf_int["mean_ci_upper"], product_id)
+                insert_model_predictions(result)
+                session.commit()
+                session.close()
+        except:
+            session.rollback()
+            session.close()
+            raise
 
 
 def main() -> None:
