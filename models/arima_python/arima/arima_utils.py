e371b0aeffc74a21746568b41b50a26836bc9c60
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-24 16:29:17 +0000

    get_model_id function in arima_utils

--- models/arima_python/arima/arima_utils.py
+++ models/arima_python/arima/arima_utils.py
@@ -4,7 +4,13 @@ from pathlib import Path
 
 from cropcore.db import connect_db, session_open, session_close
 from cropcore.constants import SQL_CONNECTION_STRING, SQL_DBNAME
-from cropcore.structure import ModelMeasureClass, ModelScenarioClass, SensorClass
+
+from cropcore.structure import (
+    ModelClass,
+    ModelMeasureClass,
+    ModelScenarioClass,
+    SensorClass,
+)
 
 
 # Relative import doesn't work if we are in same dir as this module
@@ -53,3 +59,27 @@ def get_sqlalchemy_session(connection_string=None, dbname=None):
     status, log, engine = connect_db(connection_string, dbname)
     session = session_open(engine)
     return session
+
+
+def get_model_id(model_name="arima", session=None):
+    """
+    Find the index of the specified model in the database
+
+    Parameters
+    ----------
+    model_name: str, name of e.g. Arima model in the ModelClass table in the db.
+    session: sqlalchemy session.  If None, use default db.
+
+    Returns
+    -------
+    model_id: int, or None if not found
+    """
+    if not session:
+        session = get_sqlalchemy_session()
+    query = session.query(ModelClass.id, ModelClass.model_name).filter(
+        ModelClass.model_name == model_name
+    )
+    result = session.execute(query).fetchone()
+    if result:
+        return result[0]
+    return None
